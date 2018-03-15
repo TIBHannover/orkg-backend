cee27132a0543f2482f5a1b5f95d188f26906247
Manuel Prinz <manuel.prinz@tib.eu>
2018-03-15 16:58:49 +0100

    Allow access to API for CORS-compliant browsers
    An annotation is added to allow access to the API via browsers from all
    origins. This is a development feature and should be configured via a
    profile or global configuration. See #12.

--- src/main/kotlin/eu/tib/orkg/prototype/publication/resources/Resources.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/publication/resources/Resources.kt
@@ -1,6 +1,7 @@
 package eu.tib.orkg.prototype.publication.resources
 
 import eu.tib.orkg.prototype.publication.domain.model.ArticleRepository
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/articles")
+@CrossOrigin(origins = ["*"])
 class ArticleController(
     val articleRepository: ArticleRepository
 ) {

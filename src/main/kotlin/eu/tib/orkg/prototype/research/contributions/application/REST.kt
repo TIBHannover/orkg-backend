10caec7302632783ef3421ff759a278196022ea3
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-03-15 16:06:58 +0100

    Added the cross origin annotation in REST.kt. Updated README.adoc.

--- src/main/kotlin/eu/tib/orkg/prototype/research/contributions/application/REST.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/research/contributions/application/REST.kt
@@ -1,6 +1,7 @@
 package eu.tib.orkg.prototype.research.contributions.application
 
 import eu.tib.orkg.prototype.research.contributions.domain.model.ResearchContributionRepository
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/contributions")
+@CrossOrigin(origins = ["*"])
 class ResearchContributionController(
     val repository: ResearchContributionRepository
 ) {

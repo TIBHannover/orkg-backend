c688eed9c77c360c66f1d19cce0166aaab12c7ad
Manuel Prinz <manuel.prinz@tib.eu>
2018-04-13 17:02:43 +0200

    Add user ID and creation time to graph elements

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/ObjectNode.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/ObjectNode.kt
@@ -1,12 +1,13 @@
 package eu.tib.orkg.prototype.statements.domain.model
 
+import eu.tib.orkg.prototype.authentication.domain.model.UserId
 import eu.tib.orkg.prototype.core.Entity
 import java.time.LocalDateTime
 
 sealed class ObjectNode {
     data class ObjectEntity(
         override val id: ObjectId,
-        val userId: String,
+        val userId: UserId,
         val createdAt: LocalDateTime
     ) :
         Entity<ObjectId>(id)
--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/Predicate.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/Predicate.kt
@@ -1,6 +1,12 @@
 package eu.tib.orkg.prototype.statements.domain.model
 
+import eu.tib.orkg.prototype.authentication.domain.model.UserId
 import eu.tib.orkg.prototype.core.Entity
+import java.time.LocalDateTime
 
-data class Predicate(override val id: PredicateId) :
+data class Predicate(
+    override val id: PredicateId,
+    val userId: UserId,
+    val createdAt: LocalDateTime
+) :
     Entity<PredicateId>(id)

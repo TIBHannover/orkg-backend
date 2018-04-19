8badfda0eb7e326690c466d902f71a235a95cfc9
Manuel Prinz <manuel.prinz@tib.eu>
2018-04-19 14:50:05 +0200

    Delete `ObjectID`
    The ID is not a different concept and equal to subject/resource IDs, so
    it can be deleted.

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/ObjectId.kt
+++
@@ -1,6 +0,0 @@
-package eu.tib.orkg.prototype.statements.domain.model
-
-import eu.tib.orkg.prototype.core.Identity
-
-data class ObjectId(override val value: String) :
-    Identity<String>(value)
--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/ObjectNode.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/ObjectNode.kt
@@ -1,16 +1,14 @@
 package eu.tib.orkg.prototype.statements.domain.model
 
 import eu.tib.orkg.prototype.authentication.domain.model.UserId
-import eu.tib.orkg.prototype.core.Entity
 import java.time.LocalDateTime
 
 sealed class ObjectNode {
     data class ObjectEntity(
-        override val id: ObjectId,
+        val id: ResourceId,
         val userId: UserId,
         val createdAt: LocalDateTime
-    ) :
-        Entity<ObjectId>(id)
+    )
 
     data class Literal(val value: String)
 }

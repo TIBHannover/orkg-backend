5fb867f457c5520fd80c201111ffe35b8dda4820
Manuel Prinz <manuel.prinz@tib.eu>
2018-04-19 14:33:37 +0200

    Prevent exposure of predicate ID value

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/PredicateId.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/PredicateId.kt
@@ -1,14 +1,15 @@
 package eu.tib.orkg.prototype.statements.domain.model
 
-import eu.tib.orkg.prototype.core.Identity
-
-data class PredicateId(override val value: String) :
-    Identity<String>(value) {
+data class PredicateId(private val value: String) {
     init {
         require(value.isNotEmpty()) { "Value cannot be empty" }
         require(value.isNotBlank()) { "Value cannot be blank" }
         require(value.startsWith("P")) { """Value needs to start with "P"""" }
         require(value.isAlphaNumericAfterPrefix()) { """Value starts with "P" but is not alpha-numeric afterwards""" }
+    }
+
+    override fun toString(): String {
+        return value
     }
 
     private fun String.isAlphaNumericAfterPrefix(): Boolean {

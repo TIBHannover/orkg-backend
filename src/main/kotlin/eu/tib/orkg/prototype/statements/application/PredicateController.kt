5ce42679969cd550dd37b482d612f4b532a8fac0
Manuel Prinz <manuel.prinz@tib.eu>
2018-06-25 17:15:43 +0200

    Add REST documentation for predicates

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/PredicateController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/PredicateController.kt
@@ -3,6 +3,8 @@ package eu.tib.orkg.prototype.statements.application
 import eu.tib.orkg.prototype.statements.domain.model.Predicate
 import eu.tib.orkg.prototype.statements.domain.model.PredicateId
 import eu.tib.orkg.prototype.statements.domain.model.PredicateRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -35,6 +38,7 @@ class PredicateController(private val repository: PredicateRepository) {
         repository.findByLabel(searchString)
 
     @PostMapping("/")
+    @ResponseStatus(CREATED)
     fun add(@RequestBody predicate: Predicate): Predicate {
         val (id, predicateWithId) = if (predicate.id == null) {
             val id = repository.nextIdentity()
--- src/test/kotlin/eu/tib/orkg/prototype/statements/application/PredicateControllerTest.kt
+++ src/test/kotlin/eu/tib/orkg/prototype/statements/application/PredicateControllerTest.kt
@@ -3,32 +3,71 @@ package eu.tib.orkg.prototype.statements.application
 import eu.tib.orkg.prototype.statements.domain.model.Predicate
 import eu.tib.orkg.prototype.statements.domain.model.PredicateId
 import eu.tib.orkg.prototype.statements.domain.model.PredicateRepository
+import eu.tib.orkg.prototype.statements.infrastructure.InMemoryPredicateRepository
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito.given
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
+import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @DisplayName("Predicate Controller")
 class PredicateControllerTest : RestDocumentationBaseTest() {
 
-    @MockBean
-    private lateinit var repository: PredicateRepository
+    private val repository: PredicateRepository =
+        InMemoryPredicateRepository()
 
     override fun createController() = PredicateController(repository)
 
     @Test
     fun index() {
-        given(repository.findAll()).willReturn(
-            listOf(
-                Predicate(PredicateId("P123"), "has name"),
-                Predicate(PredicateId("P987"), "knows")
-            )
-        )
+        repository.add(Predicate(PredicateId("P123"), "has name"))
+        repository.add(Predicate(PredicateId("P987"), "knows"))
 
-        getRequestTo("/api/statements/predicates/")
+        mockMvc
+            .perform(
+                get("/api/statements/predicates/")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk)
             .andDo(
-                document("predicates")
+                document(
+                    snippet,
+                    responseFields(
+                        fieldWithPath("[].id").description("The predicate ID"),
+                        fieldWithPath("[].label").description("The predicate label")
+                    )
+                )
+            )
+    }
+
+    @Test
+    fun add() {
+        val resource = mapOf("label" to "knows")
+
+        mockMvc
+            .perform(
+                post("/api/statements/predicates/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(resource))
+            )
+            .andExpect(status().isCreated)
+            .andDo(
+                document(
+                    snippet,
+                    requestFields(
+                        fieldWithPath("label").description("The predicate label")
+                    ),
+                    responseFields(
+                        fieldWithPath("id").description("The predicate ID"),
+                        fieldWithPath("label").description("The predicate label")
+                    )
+                )
             )
     }
 }

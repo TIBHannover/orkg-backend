c6935482cb825c6cba37bfc33068be3ffa31a6c7
Manuel Prinz <manuel.prinz@tib.eu>
2018-06-25 16:42:51 +0200

    Document listing resources

--- src/test/kotlin/eu/tib/orkg/prototype/statements/application/ResourceControllerTest.kt
+++ src/test/kotlin/eu/tib/orkg/prototype/statements/application/ResourceControllerTest.kt
@@ -1,31 +1,50 @@
 package eu.tib.orkg.prototype.statements.application
 
+import eu.tib.orkg.prototype.statements.domain.model.Resource
+import eu.tib.orkg.prototype.statements.domain.model.ResourceId
 import eu.tib.orkg.prototype.statements.domain.model.ResourceRepository
 import eu.tib.orkg.prototype.statements.infrastructure.InMemoryResourceRepository
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @DisplayName("Resource Controller")
 class ResourceControllerTest : RestDocumentationBaseTest() {
 
-    @MockBean
-    private lateinit var repository: ResourceRepository
-
     // FIXME: mock repository, to be independent of in-memory implementation
-    override fun createController() =
-        ResourceController(InMemoryResourceRepository())
+    private val repository: ResourceRepository =
+        InMemoryResourceRepository()
+
+    override fun createController() = ResourceController(repository)
 
     @Test
     fun index() {
-        getRequestTo("/api/statements/resources/")
+        repository.add(Resource(ResourceId("1"), "knows"))
+        repository.add(Resource(ResourceId("2"), "is a"))
+
+        mockMvc
+            .perform(
+                get("/api/statements/resources/")
+                    .accept(APPLICATION_JSON)
+                    .contentType(APPLICATION_JSON)
+            )
+            .andExpect(status().isOk)
+            .andDo(
+                document(
+                    snippet,
+                    responseFields(
+                        fieldWithPath("[].id").description("The resource ID"),
+                        fieldWithPath("[].label").description("The resource label")
+                    )
+                )
+            )
     }
 
     @Test

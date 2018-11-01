47207fb496c850d35a4b3a77cec271d6c3ac5dfb
Manuel Prinz <manuel.prinz@tib.eu>
2018-11-01 10:31:49 +0100

    Upgrade Gradle Docker plug-in to 0.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     kotlin("plugin.spring") version "1.2.71"
     id("org.springframework.boot") version "2.0.5.RELEASE"
     id("org.asciidoctor.convert") version "1.5.8.1"
-    id("com.palantir.docker") version "0.19.2"
+    id("com.palantir.docker") version "0.20.1"
     war
 }
 

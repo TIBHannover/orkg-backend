016de2b15424c5d0b26c579117ce93e65834b0ce
Manuel Prinz <manuel.prinz@tib.eu>
2018-11-10 21:24:06 +0100

    Use predicate service in predicate controller

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/PredicateController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/PredicateController.kt
@@ -1,30 +1,20 @@
 package eu.tib.orkg.prototype.statements.application
 
-import eu.tib.orkg.prototype.statements.domain.model.Predicate
-import eu.tib.orkg.prototype.statements.domain.model.PredicateId
-import eu.tib.orkg.prototype.statements.domain.model.PredicateRepository
+import eu.tib.orkg.prototype.statements.domain.model.*
+import org.springframework.http.*
 import org.springframework.http.HttpStatus.*
-import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.*
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.util.UriComponentsBuilder
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.*
 
 @RestController
 @RequestMapping("/api/predicates/")
 @CrossOrigin(origins = ["*"])
-class PredicateController(private val repository: PredicateRepository) {
+class PredicateController(private val service: PredicateService) {
 
     @GetMapping("/{id}")
     fun findById(@PathVariable id: PredicateId): Predicate =
-        repository
+        service
             .findById(id)
             .orElseThrow { ResourceNotFound() }
 
@@ -35,26 +25,20 @@ class PredicateController(private val repository: PredicateRepository) {
             required = false
         ) searchString: String?
     ) = if (searchString == null)
-        repository.findAll()
+        service.findAll()
     else
-        repository.findByLabel(searchString)
+        service.findAllByLabelContaining(searchString)
 
     @PostMapping("/")
     @ResponseStatus(CREATED)
     fun add(@RequestBody predicate: Predicate, uriComponentsBuilder: UriComponentsBuilder): ResponseEntity<Predicate> {
-        val (id, predicateWithId) = if (predicate.id == null) {
-            val id = repository.nextIdentity()
-            Pair(id, predicate.copy(id = id))
-        } else {
-            Pair(predicate.id, predicate)
-        }
-        repository.add(predicateWithId)
+        val id = service.create(predicate.label).id
 
         val location = uriComponentsBuilder
             .path("api/predicates/{id}")
             .buildAndExpand(id)
             .toUri()
 
-        return created(location).body(repository.findById(id).get())
+        return created(location).body(service.findById(id).get())
     }
 }

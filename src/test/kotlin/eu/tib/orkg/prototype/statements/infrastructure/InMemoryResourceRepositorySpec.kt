9f3b818433309d441c592c8d6652c258c52676d8
Manuel Prinz <manuel.prinz@tib.eu>
2018-11-13 14:35:30 +0100

    Remove in-memory implementations

--- src/test/kotlin/eu/tib/orkg/prototype/statements/infrastructure/InMemoryResourceRepositorySpec.kt
+++
@@ -1,9 +0,0 @@
-package eu.tib.orkg.prototype.statements.infrastructure
-
-import eu.tib.orkg.prototype.statements.domain.model.ResourceRepositorySpec
-import org.junit.jupiter.api.DisplayName
-
-@DisplayName("In-Memory Resource Repository")
-class InMemoryResourceRepositorySpec : ResourceRepositorySpec() {
-    override fun createRepository() = InMemoryResourceRepository()
-}

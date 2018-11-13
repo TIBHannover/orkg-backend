bf5eefc7d655ccbf0aa017d438a6ebcd3d201b0e
Manuel Prinz <manuel.prinz@tib.eu>
2018-11-13 13:59:11 +0100

    Use statement service in controller

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/StatementController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/StatementController.kt
@@ -1,48 +1,42 @@
 package eu.tib.orkg.prototype.statements.application
 
-import eu.tib.orkg.prototype.statements.domain.model.Object
-import eu.tib.orkg.prototype.statements.domain.model.PredicateId
-import eu.tib.orkg.prototype.statements.domain.model.ResourceId
-import eu.tib.orkg.prototype.statements.domain.model.Statement
-import eu.tib.orkg.prototype.statements.domain.model.StatementRepository
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpStatus.CREATED
-import org.springframework.http.ResponseEntity.created
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.util.UriComponentsBuilder
+import eu.tib.orkg.prototype.statements.domain.model.*
+import org.springframework.http.*
+import org.springframework.http.HttpStatus.*
+import org.springframework.http.ResponseEntity.*
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.*
 
 @RestController
 @RequestMapping("/api/statements")
 @CrossOrigin(origins = ["*"])
-class StatementController(private val repository: StatementRepository) {
+class StatementController(private val service: StatementWithResourceService) {
 
     @GetMapping("/")
-    fun findAll(): Iterable<Statement> {
-        return repository.findAll()
+    fun findAll(): Iterable<StatementWithResource> {
+        return service.findAll()
     }
 
     @GetMapping("/{statementId}")
     fun findById(@PathVariable statementId: Long) =
-        repository.findById(statementId)
+        service.findById(statementId)
 
     @GetMapping("/subject/{resourceId}")
     fun findByResource(@PathVariable resourceId: ResourceId) =
-        repository.findBySubject(resourceId)
+        service.findAllBySubject(resourceId)
 
     @GetMapping("/predicate/{predicateId}")
     fun findByPredicate(@PathVariable predicateId: PredicateId) =
-        repository.findByPredicate(predicateId)
+        service.findAllByPredicate(predicateId)
 
     @PostMapping("/")
-    fun add(@RequestBody statement: Statement) =
-        repository.add(statement)
+    // FIXME: how can we deal with that without null issues?
+    fun add(@RequestBody statement: StatementWithResource) =
+        service.create(
+            statement.subject.id!!,
+            statement.predicate.id!!,
+            statement.`object`.id!!
+        )
 
     @PostMapping("/{subjectId}/{predicateId}/{objectId}")
     @ResponseStatus(CREATED)
@@ -51,46 +45,13 @@ class StatementController(private val repository: StatementRepository) {
         @PathVariable predicateId: PredicateId,
         @PathVariable objectId: ResourceId,
         uriComponentsBuilder: UriComponentsBuilder
-    ): HttpEntity<Statement> {
-        val statement = Statement(
-            statementId = repository.nextIdentity(),
-            subject = subjectId,
-            predicate = predicateId,
-            `object` = Object.Resource(objectId)
-        )
-
+    ): HttpEntity<StatementWithResource> {
         // TODO: should error if parts not found?
-        repository.add(statement)
+        val statement = service.create(subjectId, predicateId, objectId)
 
         val location = uriComponentsBuilder
             .path("api/statements/{id}")
-            .buildAndExpand(statement.statementId)
-            .toUri()
-
-        return created(location).body(statement)
-    }
-
-    @PostMapping("/{subjectId}/{predicateId}")
-    @ResponseStatus(CREATED)
-    fun createWithObjectLiteral(
-        @PathVariable subjectId: ResourceId,
-        @PathVariable predicateId: PredicateId,
-        @RequestBody `object`: Object.Literal,
-        uriComponentsBuilder: UriComponentsBuilder
-    ): HttpEntity<Statement> {
-        val statement = Statement(
-            statementId = repository.nextIdentity(),
-            subject = subjectId,
-            predicate = predicateId,
-            `object` = `object`
-        )
-
-        // TODO: should error if parts not found?
-        repository.add(statement)
-
-        val location = uriComponentsBuilder
-            .path("api/statements/{id}")
-            .buildAndExpand(statement.statementId)
+            .buildAndExpand(statement.id)
             .toUri()
 
         return created(location).body(statement)
--- src/test/kotlin/eu/tib/orkg/prototype/statements/application/StatementControllerTest.kt
+++ src/test/kotlin/eu/tib/orkg/prototype/statements/application/StatementControllerTest.kt
@@ -1,53 +1,46 @@
 package eu.tib.orkg.prototype.statements.application
 
-import eu.tib.orkg.prototype.statements.domain.model.Object
-import eu.tib.orkg.prototype.statements.domain.model.PredicateId
-import eu.tib.orkg.prototype.statements.domain.model.ResourceId
-import eu.tib.orkg.prototype.statements.domain.model.Statement
-import eu.tib.orkg.prototype.statements.domain.model.StatementRepository
-import eu.tib.orkg.prototype.statements.infrastructure.InMemoryStatementRepository
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.springframework.http.MediaType
-import org.springframework.restdocs.headers.HeaderDocumentation.headerWithName
-import org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import eu.tib.orkg.prototype.statements.domain.model.*
+import eu.tib.orkg.prototype.statements.infrastructure.*
+import org.junit.jupiter.api.*
+import org.springframework.beans.factory.annotation.*
+import org.springframework.http.*
+import org.springframework.restdocs.headers.HeaderDocumentation.*
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post
-import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
-import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
-import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
-import org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath
-import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
-import org.springframework.restdocs.request.RequestDocumentation.pathParameters
+import org.springframework.restdocs.payload.PayloadDocumentation.*
+import org.springframework.restdocs.request.RequestDocumentation.*
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @DisplayName("Statement Controller")
 class StatementControllerTest : RestDocumentationBaseTest() {
 
-    private val repository: StatementRepository =
-        InMemoryStatementRepository()
+    override fun createController() = controller
 
-    override fun createController() = StatementController(repository)
+    @Autowired
+    private lateinit var statementService: StatementWithResourceService
+
+    @Autowired
+    private lateinit var resourceService: ResourceService
+
+    @Autowired
+    private lateinit var predicateService: PredicateService
+
+    @Autowired
+    private lateinit var controller: StatementController
 
     @Test
     fun index() {
-        repository.add(
-            Statement(
-                repository.nextIdentity(),
-                ResourceId("123"),
-                PredicateId("P576"),
-                Object.Resource(ResourceId("789"))
-            )
-        )
-        repository.add(
-            Statement(
-                repository.nextIdentity(),
-                ResourceId("123"),
-                PredicateId("P432"),
-                Object.Resource(ResourceId("633"))
-            )
-        )
+        val r1 = resourceService.create("one")
+        val r2 = resourceService.create("two")
+        val r3 = resourceService.create("three")
+        val p1 = predicateService.create("blah")
+        val p2 = predicateService.create("blub")
+
+        statementService.create(r1.id!!, p1.id!!, r2.id!!)
+        statementService.create(r1.id!!, p2.id!!, r3.id!!)
+        statementService.create(r1.id!!, p2.id!!, r3.id!!)
 
         mockMvc
             .perform(
@@ -60,18 +53,16 @@ class StatementControllerTest : RestDocumentationBaseTest() {
                 document(
                     snippet,
                     responseFields(
-                        fieldWithPath("[].statementId").description(
-                            "The statement ID"
-                        ),
-                        fieldWithPath("[].subject").description(
-                            "The resource ID"
-                        ),
-                        fieldWithPath("[].predicate").description(
-                            "The predicate ID"
-                        ),
-                        subsectionWithPath("[].object").description(
-                            "The type of object"
-                        )
+                        fieldWithPath("[].id").description("The statement ID"),
+                        fieldWithPath("[].subject").description("A resource"),
+                        fieldWithPath("[].subject.id").description("The ID of the subject resource"),
+                        fieldWithPath("[].subject.label").description("The label of the subject resource"),
+                        fieldWithPath("[].predicate").description("A predicate"),
+                        fieldWithPath("[].predicate.id").description("The ID of the predicate"),
+                        fieldWithPath("[].predicate.label").description("The label of the predicate"),
+                        fieldWithPath("[].object").description("An object"),
+                        fieldWithPath("[].object.id").description("The ID of the object"),
+                        fieldWithPath("[].object.label").description("The label of the object")
                     )
                 )
             )
@@ -79,26 +70,19 @@ class StatementControllerTest : RestDocumentationBaseTest() {
 
     @Test
     fun fetch() {
-        repository.add(
-            Statement(
-                1,
-                ResourceId("123"),
-                PredicateId("P576"),
-                Object.Resource(ResourceId("789"))
-            )
-        )
-        repository.add(
-            Statement(
-                2,
-                ResourceId("123"),
-                PredicateId("P432"),
-                Object.Resource(ResourceId("633"))
-            )
-        )
+        val r1 = resourceService.create("one")
+        val r2 = resourceService.create("two")
+        val r3 = resourceService.create("three")
+        val p1 = predicateService.create("blah")
+        val p2 = predicateService.create("blub")
+
+        val statement = statementService.create(r1.id!!, p1.id!!, r2.id!!)
+        statementService.create(r1.id!!, p1.id!!, r3.id!!)
+        statementService.create(r1.id!!, p2.id!!, r3.id!!)
 
         mockMvc
             .perform(
-                get("/api/statements/2")
+                get("/api/statements/${statement.id}")
                     .accept(MediaType.APPLICATION_JSON)
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -107,18 +91,16 @@ class StatementControllerTest : RestDocumentationBaseTest() {
                 document(
                     snippet,
                     responseFields(
-                        fieldWithPath("statementId").description(
-                            "The statement ID"
-                        ),
-                        fieldWithPath("subject").description(
-                            "The resource ID"
-                        ),
-                        fieldWithPath("predicate").description(
-                            "The predicate ID"
-                        ),
-                        subsectionWithPath("object").description(
-                            "The type of object"
-                        )
+                        fieldWithPath("id").description("The statement ID"),
+                        fieldWithPath("subject").description("A resource"),
+                        fieldWithPath("subject.id").description("The ID of the subject resource"),
+                        fieldWithPath("subject.label").description("The label of the subject resource"),
+                        fieldWithPath("predicate").description("A predicate"),
+                        fieldWithPath("predicate.id").description("The ID of the predicate"),
+                        fieldWithPath("predicate.label").description("The label of the predicate"),
+                        fieldWithPath("object").description("An object"),
+                        fieldWithPath("object.id").description("The ID of the object"),
+                        fieldWithPath("object.label").description("The label of the object")
                     )
                 )
             )
@@ -126,26 +108,18 @@ class StatementControllerTest : RestDocumentationBaseTest() {
 
     @Test
     fun lookupBySubject() {
-        repository.add(
-            Statement(
-                repository.nextIdentity(),
-                ResourceId("123"),
-                PredicateId("P576"),
-                Object.Resource(ResourceId("789"))
-            )
-        )
-        repository.add(
-            Statement(
-                repository.nextIdentity(),
-                ResourceId("123"),
-                PredicateId("P432"),
-                Object.Resource(ResourceId("633"))
-            )
-        )
+        val r1 = resourceService.create("one")
+        val r2 = resourceService.create("two")
+        val r3 = resourceService.create("three")
+        val p1 = predicateService.create("blah")
+        val p2 = predicateService.create("blub")
+
+        statementService.create(r1.id!!, p1.id!!, r2.id!!)
+        statementService.create(r1.id!!, p2.id!!, r3.id!!)
 
         mockMvc
             .perform(
-                get("/api/statements/subject/123")
+                get("/api/statements/subject/${r1.id}")
                     .accept(MediaType.APPLICATION_JSON)
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -154,18 +128,16 @@ class StatementControllerTest : RestDocumentationBaseTest() {
                 document(
                     snippet,
                     responseFields(
-                        fieldWithPath("[].statementId").description(
-                            "The statement ID"
-                        ),
-                        fieldWithPath("[].subject").description(
-                            "The resource ID"
-                        ),
-                        fieldWithPath("[].predicate").description(
-                            "The predicate ID"
-                        ),
-                        subsectionWithPath("[].object").description(
-                            "The type of object"
-                        )
+                        fieldWithPath("[].id").description("The statement ID"),
+                        fieldWithPath("[].subject").description("A resource"),
+                        fieldWithPath("[].subject.id").description("The ID of the subject resource"),
+                        fieldWithPath("[].subject.label").description("The label of the subject resource"),
+                        fieldWithPath("[].predicate").description("A predicate"),
+                        fieldWithPath("[].predicate.id").description("The ID of the predicate"),
+                        fieldWithPath("[].predicate.label").description("The label of the predicate"),
+                        fieldWithPath("[].object").description("An object"),
+                        fieldWithPath("[].object.id").description("The ID of the object"),
+                        fieldWithPath("[].object.label").description("The label of the object")
                     )
                 )
             )
@@ -173,26 +145,19 @@ class StatementControllerTest : RestDocumentationBaseTest() {
 
     @Test
     fun lookupByPredicate() {
-        repository.add(
-            Statement(
-                repository.nextIdentity(),
-                ResourceId("123"),
-                PredicateId("P576"),
-                Object.Resource(ResourceId("789"))
-            )
-        )
-        repository.add(
-            Statement(
-                repository.nextIdentity(),
-                ResourceId("345"),
-                PredicateId("P576"),
-                Object.Resource(ResourceId("633"))
-            )
-        )
+        val r1 = resourceService.create("one")
+        val r2 = resourceService.create("two")
+        val r3 = resourceService.create("three")
+        val p1 = predicateService.create("blah")
+        val p2 = predicateService.create("blub")
+
+        statementService.create(r1.id!!, p1.id!!, r2.id!!)
+        statementService.create(r1.id!!, p1.id!!, r3.id!!)
+        statementService.create(r1.id!!, p2.id!!, r3.id!!)
 
         mockMvc
             .perform(
-                get("/api/statements/predicate/P576")
+                get("/api/statements/predicate/${p1.id}")
                     .accept(MediaType.APPLICATION_JSON)
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -201,18 +166,16 @@ class StatementControllerTest : RestDocumentationBaseTest() {
                 document(
                     snippet,
                     responseFields(
-                        fieldWithPath("[].statementId").description(
-                            "The statement ID"
-                        ),
-                        fieldWithPath("[].subject").description(
-                            "The resource ID"
-                        ),
-                        fieldWithPath("[].predicate").description(
-                            "The predicate ID"
-                        ),
-                        subsectionWithPath("[].object").description(
-                            "The type of object"
-                        )
+                        fieldWithPath("[].id").description("The statement ID"),
+                        fieldWithPath("[].subject").description("A resource"),
+                        fieldWithPath("[].subject.id").description("The ID of the subject resource"),
+                        fieldWithPath("[].subject.label").description("The label of the subject resource"),
+                        fieldWithPath("[].predicate").description("A predicate"),
+                        fieldWithPath("[].predicate.id").description("The ID of the predicate"),
+                        fieldWithPath("[].predicate.label").description("The label of the predicate"),
+                        fieldWithPath("[].object").description("An object"),
+                        fieldWithPath("[].object.id").description("The ID of the object"),
+                        fieldWithPath("[].object.label").description("The label of the object")
                     )
                 )
             )
@@ -220,12 +183,16 @@ class StatementControllerTest : RestDocumentationBaseTest() {
 
     @Test
     fun addWithResource() {
+        val r1 = resourceService.create("one")
+        val r2 = resourceService.create("two")
+        val p = predicateService.create("less than")
+
         mockMvc.perform(
             post(
                 "/api/statements/{subject}/{predicate}/{object}",
-                "123",
-                "P234",
-                "345"
+                r1.id,
+                p.id,
+                r2.id
             )
                 .contentType(MediaType.APPLICATION_JSON)
         )
@@ -237,40 +204,6 @@ class StatementControllerTest : RestDocumentationBaseTest() {
                         parameterWithName("subject").description("The resource ID describing the subject"),
                         parameterWithName("predicate").description("The predicate ID describing the predicate"),
                         parameterWithName("object").description("The resource ID describing the object")
-                    ),
-                    responseHeaders(
-                        headerWithName("Location").description("Location to the created statement")
-                    )
-                )
-            )
-    }
-
-    @Test
-    fun addWithLiteral() {
-        val value = mapOf(
-            "value" to "some value",
-            "type" to "literal"
-        )
-        mockMvc.perform(
-            post(
-                "/api/statements/{subject}/{predicate}",
-                "123",
-                "P234"
-            )
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(value))
-        )
-            .andExpect(status().isCreated)
-            .andDo(
-                document(
-                    snippet,
-                    pathParameters(
-                        parameterWithName("subject").description("The resource ID describing the subject"),
-                        parameterWithName("predicate").description("The predicate ID describing the predicate")
-                    ),
-                    requestFields(
-                        fieldWithPath("value").description("The literal value"),
-                        fieldWithPath("type").description("The type of object. Must be \"literal\".")
                     ),
                     responseHeaders(
                         headerWithName("Location").description("Location to the created statement")

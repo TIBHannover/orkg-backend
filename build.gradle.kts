4770328f0b59d9fc2d0f3311b66c37a8b0c4a7b8
Manuel Prinz <manuel.prinz@tib.eu>
2019-02-08 11:42:11 +0100

    Upgrade to Kotlin 1.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ val kotlinVersion = plugins.getPlugin(KotlinPluginWrapper::class.java)
 val neo4jVersion = "3.3.+" // should match version in Dockerfile
 
 plugins {
-    kotlin("jvm") version "1.3.0"
-    kotlin("plugin.spring") version "1.3.0"
+    kotlin("jvm") version "1.3.21"
+    kotlin("plugin.spring") version "1.3.21"
     id("org.springframework.boot") version "2.1.2.RELEASE"
     id("org.asciidoctor.convert") version "1.5.8.1"
     id("com.palantir.docker") version "0.20.1"

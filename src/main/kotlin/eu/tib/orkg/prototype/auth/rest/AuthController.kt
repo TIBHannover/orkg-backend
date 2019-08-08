9c30e3d4267ad8dfcb84600d97b129267a9f1da7
Manuel Prinz <manuel.prinz@tib.eu>
2019-08-08 11:19:38 +0200

    Add user registration

---
+++ src/main/kotlin/eu/tib/orkg/prototype/auth/rest/AuthController.kt
@@ -0,0 +1,48 @@
+package eu.tib.orkg.prototype.auth.rest
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import eu.tib.orkg.prototype.auth.service.UserService
+import org.springframework.http.HttpStatus.OK
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import java.lang.RuntimeException
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = ["*"])
+class AuthController(
+    private val userService: UserService
+) {
+    @PostMapping("/register")
+    @ResponseStatus(OK)
+    fun registerUser(@RequestBody request: RegisterUserRequest): ResponseEntity<*> {
+        val user = userService.findByEmail(request.email)
+        if (user.isPresent) {
+            return ok(RegisteredUserResponse("success"))
+        }
+
+        if (request.password != request.matchingPassword)
+            throw RuntimeException("Passwords do not match")
+
+        userService.registerUser(request.email, request.password)
+
+        return ok(RegisteredUserResponse("success"))
+    }
+
+    data class RegisterUserRequest(
+        val email: String,
+        val password: String,
+        @JsonProperty("matching_password")
+        val matchingPassword: String
+    )
+
+    data class RegisteredUserResponse(
+        val status: String
+    )
+}
---
+++ src/main/kotlin/eu/tib/orkg/prototype/auth/service/UserService.kt
@@ -0,0 +1,35 @@
+package eu.tib.orkg.prototype.auth.service
+
+import eu.tib.orkg.prototype.auth.persistence.RoleEntity
+import eu.tib.orkg.prototype.auth.persistence.UserEntity
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.Optional
+import java.util.UUID
+
+@Service
+@Transactional
+class UserService(
+    private val repository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) {
+    fun findByEmail(email: String): Optional<UserEntity> {
+        return repository.findByEmail(email)
+    }
+
+    fun registerUser(anEmail: String, aPassword: String) {
+        val userId = UUID.randomUUID()
+        val newUser = UserEntity().apply {
+            id = userId
+            email = anEmail
+            password = passwordEncoder.encode(aPassword)
+            enabled = true
+            roles = mutableSetOf(RoleEntity().apply {
+                name = "ROLE_USER"
+                id = userId
+            })
+        }
+        repository.save(newUser)
+    }
+}

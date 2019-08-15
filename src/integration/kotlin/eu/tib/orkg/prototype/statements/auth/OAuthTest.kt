d1c7f463a0e883c6176a0225c83d29e9f91b2a7f
Manuel Prinz <manuel.prinz@tib.eu>
2019-08-15 17:08:16 +0200

    Remove database initialization code from auth test
    This is not needed anymore, as the database is configured via
    `application.yaml`.

--- src/integration/kotlin/eu/tib/orkg/prototype/statements/auth/OAuthTest.kt
+++ src/integration/kotlin/eu/tib/orkg/prototype/statements/auth/OAuthTest.kt
@@ -10,17 +10,11 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.boot.test.util.TestPropertyValues
 import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.context.ApplicationContextInitializer
-import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.testcontainers.containers.PostgreSQLContainer
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ExtendWith(SpringExtension::class)
-@ContextConfiguration(initializers = [OAuthTest.Initializer::class])
 @AutoConfigureTestDatabase(replace = NONE)
 class OAuthTest {
     @Autowired
@@ -37,32 +31,4 @@ class OAuthTest {
         val token = OrkgApiClient(port).getAccessToken("user@example.org", "user")
         assertThat(token).isNotNull()
     }
-
-    class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
-        override fun initialize(applicationContext: ConfigurableApplicationContext) {
-            postgres.start()
-            TestPropertyValues.of(
-                "spring.datasource.url=${postgres.jdbcUrl}",
-                "spring.datasource.username=${postgres.username}",
-                "spring.datasource.password=${postgres.password}",
-                "spring.datasource.driver-class-name=org.postgresql.Driver"
-            ).applyTo(applicationContext.environment)
-        }
-
-        companion object {
-            private val postgres: KPostgreSQLContainer by lazy {
-                KPostgreSQLContainer("postgres:11").apply {
-                    withDatabaseName("postgres")
-                    withUsername("postgres")
-                    withPassword("postgres")
-                }
-            }
-        }
-    }
 }
-
-/**
- * This is a class used as a work-around. Kotlin is "confused" by the use of Generics in
- * the TestContainers library. Generating a sub-class avoids this problem.
- */
-class KPostgreSQLContainer(imageName: String) : PostgreSQLContainer<KPostgreSQLContainer>(imageName)

3364eda84c9585a4c29d2ce1b29c1f71018915c4
Manuel Prinz <manuel.prinz@tib.eu>
2019-08-16 16:44:56 +0200

    Add global CORS configuration

--- src/main/kotlin/eu/tib/orkg/prototype/SpringApplication.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/SpringApplication.kt
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.context.annotation.Bean
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -15,6 +19,20 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
 class Application : SpringBootServletInitializer() {
     override fun configure(builder: SpringApplicationBuilder?) =
         builder?.sources(Application::class.java)
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val config = CorsConfiguration().apply {
+            allowCredentials = true
+            addAllowedOrigin("http://localhost:8000")
+            addAllowedHeader("*")
+            addAllowedMethod("*")
+        }
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
+        return CorsFilter(source)
+    }
 }
 
 fun main(args: Array<String>) {

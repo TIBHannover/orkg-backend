609fadaafc9c7e5f1086e1ffdc50288c0f1d7af8
YaserJaradeh <yaser.jaradeh@tib.eu>
2019-09-30 10:58:06 +0200

    Paper Controller to handle the call

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/PaperController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/PaperController.kt
@@ -1,6 +1,8 @@
 package eu.tib.orkg.prototype.statements.application
 
+import eu.tib.orkg.prototype.statements.domain.model.LiteralId
 import eu.tib.orkg.prototype.statements.domain.model.LiteralService
+import eu.tib.orkg.prototype.statements.domain.model.PredicateId
 import eu.tib.orkg.prototype.statements.domain.model.PredicateService
 import eu.tib.orkg.prototype.statements.domain.model.Resource
 import eu.tib.orkg.prototype.statements.domain.model.ResourceId
@@ -17,6 +19,16 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.util.UriComponentsBuilder
 
+
+const val ID_ISA_PREDICATE = "P3"
+const val ID_PAPER_RESOURCE = "R10"
+const val ID_DOI_PREDICATE = "P26"
+const val ID_AUTHOR_PREDICATE = "P27"
+const val ID_PUBDATE_MONTH_PREDICATE = "P28"
+const val ID_PUBDATE_YEAR_PREDICATE = "P29"
+const val ID_RESEARCH_FIELD_PREDICATE = "P30"
+const val ID_CONTRIBUTION_PREDICATE = "P31"
+
 @RestController
 @RequestMapping("/api/papers/")
 @CrossOrigin(origins = ["*"])
@@ -28,20 +40,165 @@ class PaperController(private val resourceService: ResourceService, private val 
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     fun add(@RequestBody paper: CreatePaperRequest, uriComponentsBuilder: UriComponentsBuilder): ResponseEntity<Resource> {
+        return ResponseEntity.accepted().body(insertData(paper))
+    }
 
+    fun insertData(paper: CreatePaperRequest): Resource {
+        // consts
+        val isAPredicate = predicateService.findById(PredicateId(ID_ISA_PREDICATE)).get().id!!
+        val hasDoiPredicate = predicateService.findById(PredicateId(ID_DOI_PREDICATE)).get().id!!
+        val hasAuthorPredicate = predicateService.findById(PredicateId(ID_AUTHOR_PREDICATE)).get().id!!
+        val publicationMonthPredicate = predicateService.findById(PredicateId(ID_PUBDATE_MONTH_PREDICATE)).get().id!!
+        val publicationYearPredicate = predicateService.findById(PredicateId(ID_PUBDATE_YEAR_PREDICATE)).get().id!!
+        val researchFieldPredicate = predicateService.findById(PredicateId(ID_RESEARCH_FIELD_PREDICATE)).get().id!!
+        val hasContributionPredicate = predicateService.findById(PredicateId(ID_CONTRIBUTION_PREDICATE)).get().id!!
 
-        return ResponseEntity.accepted().body(resourceService.findById(ResourceId("R10")).get())
+        val predicates: HashMap<String, PredicateId> = HashMap<String, PredicateId>()
+        if (paper.predicates != null)
+        {
+            paper.predicates.forEach {
+                val surrogateId = it[it.keys.first()]!!
+                val predicateId = predicateService.create(it.keys.first()).id!!
+                predicates[surrogateId] = predicateId
+            }
+        }
+
+        // paper title
+        val paperObj = resourceService.create(paper.paper.title)
+        val paperId = paperObj.id!!
+        statementWithResourceService.create(paperId, isAPredicate, ResourceId(ID_PAPER_RESOURCE))
+
+        // paper doi
+        if (paper.paper.doi != null)
+        {
+            val paperDoi = literalService.create(paper.paper.doi).id!!
+            statementWithLiteralService.create(paperId, hasDoiPredicate, paperDoi)
+        }
+
+        // paper authors
+        paper.paper.authors!!.forEach{
+            val authorId = if (it.id == null) {
+                resourceService.create(it.label!!).id!!
+            } else {
+                ResourceId(it.id)
+            }
+            statementWithResourceService.create(paperId, hasAuthorPredicate, authorId)
+        }
+
+        // paper publication date
+        if (paper.paper.publicationMonth != null)
+            statementWithLiteralService.create(
+                paperId,
+                publicationMonthPredicate,
+                literalService.create(paper.paper.publicationMonth.toString()).id!!
+            )
+        if (paper.paper.publicationYear != null)
+            statementWithLiteralService.create(
+                paperId,
+                publicationYearPredicate,
+                literalService.create(paper.paper.publicationYear.toString()).id!!
+            )
+
+        // paper research field
+        statementWithResourceService.create(paperId, researchFieldPredicate, ResourceId(paper.paper.researchField))
+
+        val tempResources: HashMap<String, String> = HashMap()
+
+        // paper contribution data
+        paper.paper.contributions!!.forEach {
+            if (it.values!= null && it.values.count() > 0)
+            {
+                val contributionId = resourceService.create(it.name).id!!
+                statementWithResourceService.create(paperId, hasContributionPredicate, contributionId)
+
+                processContributionData(contributionId, it.values, tempResources, predicates)
+            }
+        }
+
+        return paperObj
+    }
+
+    fun processContributionData(subject: ResourceId,
+                                data: HashMap<String, List<PaperValue>>,
+                                tempResources: HashMap<String, String>,
+                                predicates: HashMap<String, PredicateId>) {
+
+        for ((predicate, value) in data) {
+            val predicateId = if (predicate.startsWith("_")) {
+                predicates[predicate]
+            } else {
+                PredicateId(predicate) //
+            }
+            for (resource in value) {
+                when {
+                    resource.`@id` != null -> { // Add an existing resource or literal
+                        when {
+                            resource.`@id`.startsWith("L") -> {
+                                statementWithLiteralService.create(subject, predicateId!!, LiteralId(resource.`@id`))
+                            }
+                            resource.`@id`.startsWith("R") -> {
+                                statementWithResourceService.create(subject, predicateId!!, ResourceId(resource.`@id`))
+                            }
+                            resource.`@id`.startsWith("_") -> {
+                                // TODO: Check this
+                                // need to check if the resource id exists in the map
+                                // o/w put it in a queue and then loop over the queue somehow
+                                // the loop needs to take into account that multiple iterations on the queue might be needed
+                                val tempId = tempResources[resource.`@id`]
+                                if (tempId!!.startsWith("L")) {
+                                    statementWithLiteralService.create(subject, predicateId!!, LiteralId(tempId))
+                                } else {
+                                    statementWithResourceService.create(subject, predicateId!!, ResourceId(tempId))
+                                }
+                            }
+                        }
+                    }
+                    resource.label != null -> { // create new resource
+                        val newResource = resourceService.create(resource.label).id!!
+                        if (resource.`@temp` != null) {
+                            tempResources[resource.`@temp`] = newResource.value
+                        }
+                        statementWithResourceService.create(subject, predicateId!!, newResource)
+                        if (resource.values != null) { // TODO: This might be extracted (or should) to the outside of when
+                            processContributionData(newResource, resource.values, tempResources, predicates)
+                        }
+                    }
+                    resource.text != null -> { // create new literal
+                        val newLiteral = literalService.create(resource.text).id!!
+                        if (resource.`@temp` != null) {
+                            tempResources[resource.`@temp`] = newLiteral.value
+                        }
+                        statementWithLiteralService.create(subject, predicateId!!, newLiteral)
+                    }
+                }
+            }
+        }
     }
 }
 
 data class CreatePaperRequest(
-    val predicates: List<HashMap<String,String>>?,
+    val predicates: List<HashMap<String, String>>?,
     val paper: Paper
 )
 
 data class Paper(
-    val `@title`: String,
-    val values: HashMap<String,List<PaperValue>>?
+    val title: String,
+    val doi: String?,
+    val authors: List<Author>?,
+    val publicationMonth : Int?,
+    val publicationYear : Int?,
+    val researchField: String,
+    val contributions: List<Contribution>?
+)
+
+data class Author(
+    val id: String?,
+    val label: String?
+)
+
+data class Contribution(
+    val name: String,
+    val values: HashMap<String, List<PaperValue>>?
 )
 
 data class PaperValue(
@@ -49,5 +206,5 @@ data class PaperValue(
     val `@temp`: String?,
     val text: String?,
     val label: String?,
-    val values: HashMap<String,List<PaperValue>>?
+    val values: HashMap<String, List<PaperValue>>?
 )

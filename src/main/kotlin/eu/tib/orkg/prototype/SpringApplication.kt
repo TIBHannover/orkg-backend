e58ed6e581fa7b76993c4186ca35ccba636e0c87
Manuel Prinz <manuel.prinz@tib.eu>
2019-11-04 16:14:56 +0100

    Stop using Spring Data Auditing feature
    The Spring Data annotation to add creation dates automatically does not
    work properly. It seems that not all OGM data types are supported, only
    some from Joda Time. Adding the dates during creation time works.
    
    This commit also removed the DateTimeProvider, as it is not necessary
    anymore.

--- src/main/kotlin/eu/tib/orkg/prototype/SpringApplication.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/SpringApplication.kt
@@ -8,23 +8,16 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
 import org.springframework.core.Ordered
-import org.springframework.data.auditing.DateTimeProvider
-import org.springframework.data.neo4j.annotation.EnableNeo4jAuditing
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories
-import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
-import java.time.OffsetDateTime
-import java.time.temporal.TemporalAccessor
-import java.util.Optional
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableNeo4jRepositories("eu.tib.orkg.prototype.statements.domain.model.neo4j")
 @EntityScan("eu.tib.orkg.prototype.statements.domain.model.neo4j")
-@EnableNeo4jAuditing(dateTimeProviderRef = "zonedDateTimeProvider")
 class Application : SpringBootServletInitializer() {
     override fun configure(builder: SpringApplicationBuilder?) =
         builder?.sources(Application::class.java)
@@ -48,13 +41,4 @@ class Application : SpringBootServletInitializer() {
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
-}
-
-@Component("zonedDateTimeProvider")
-class ZonedDateTimeProvider : DateTimeProvider {
-    override fun getNow(): Optional<TemporalAccessor> {
-        // For future updates, any change to the date format can be done here
-        // and reflected on the type of the Date object in the separate classes
-        return Optional.of(OffsetDateTime.now())
-    }
 }
--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/AuditableEntity.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/AuditableEntity.kt
@@ -1,11 +1,9 @@
 package eu.tib.orkg.prototype.statements.domain.model.neo4j
 
 import org.neo4j.ogm.annotation.Property
-import org.springframework.data.annotation.CreatedDate
 import java.time.OffsetDateTime
 
 open class AuditableEntity(
     @Property("created_at")
-    @CreatedDate
     var createdAt: OffsetDateTime? = OffsetDateTime.now()
 )

92d039006137f1f884aa9ec9e1e8238459deb654
Manuel Prinz <manuel.prinz@tib.eu>
2019-11-06 15:41:33 +0100

    Disable PaperControllerTest

--- src/integration/kotlin/eu/tib/orkg/prototype/statements/application/PaperControllerTest.kt
+++ src/integration/kotlin/eu/tib/orkg/prototype/statements/application/PaperControllerTest.kt
@@ -6,7 +6,9 @@ import eu.tib.orkg.prototype.statements.domain.model.PredicateId
 import eu.tib.orkg.prototype.statements.domain.model.PredicateService
 import eu.tib.orkg.prototype.statements.domain.model.ResourceId
 import eu.tib.orkg.prototype.statements.domain.model.ResourceService
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
@@ -32,7 +34,8 @@ class PaperControllerTest : RestDocumentationBaseTest() {
 
     override fun createController() = controller
 
-    // @Test
+    @Test
+    @Disabled("Broken, due to problems with fixed IDs. See GitLab issue #94.")
     fun add() {
         service.create(CreatePredicateRequest(PredicateId("P26"), "Has DOI"))
         service.create(CreatePredicateRequest(PredicateId("P27"), "Has Author"))

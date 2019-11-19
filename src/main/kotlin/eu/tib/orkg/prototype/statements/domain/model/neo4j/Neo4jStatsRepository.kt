40d7bd5abed6f6054657a6c47fb00acf07697141
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2019-11-19 14:47:01 +0100

    Remove unnecessary cypher calls and get them from APOC meta stats

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jStatsRepository.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jStatsRepository.kt
@@ -8,12 +8,6 @@ interface Neo4jStatsRepository : Neo4jRepository<Neo4jResource, Long> {
     @Query("""CALL apoc.meta.stats()""")
     fun getGraphMetaData(): Iterable<HashMap<String, Any>>
 
-    @Query("""Match()-[:RELATES_TO{predicate_id:'P31'}]->(cont) WITH COUNT(cont) as contibutions RETURN contibutions""")
-    fun getContributionsCount(): Long
-
     @Query("""MATCH (n:Resource{resource_id: 'R11'}) CALL apoc.path.subgraphAll(n, {relationshipFilter: '>'}) YIELD nodes UNWIND nodes as field WITH COUNT(field) as cnt RETURN cnt""")
     fun getResearchFieldsCount(): Long
-
-    @Query("""MATCH ()-[{predicate_id:'P32'}]->(n) RETURN COUNT(DISTINCT n.resource_id)""")
-    fun getResearchProblemsCount(): Long
 }

7019ddcaa3008b46402ced03e9acfa5db12bd674
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2019-11-26 17:06:56 +0100

    Support Editing statements in bulk

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/bulk/BulkStatementController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/bulk/BulkStatementController.kt
@@ -2,6 +2,7 @@ package eu.tib.orkg.prototype.statements.application.bulk
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import eu.tib.orkg.prototype.statements.application.StatementController
+import eu.tib.orkg.prototype.statements.application.StatementEditRequest
 import eu.tib.orkg.prototype.statements.application.StatementResponse
 import eu.tib.orkg.prototype.statements.domain.model.ResourceId
 import eu.tib.orkg.prototype.statements.domain.model.StatementId
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -24,23 +27,23 @@ class BulkStatementController(
     @GetMapping("/subjects")
     fun findBySubjects(
         @RequestParam("ids") resourceIds: List<ResourceId>
-    ): ResponseEntity<Iterable<BulkStatementResponse>> {
+    ): ResponseEntity<Iterable<BulkGetStatementsResponse>> {
         val page = 1
         val items = 99999
         val by = "id"
         val sort = false
-        return ok(resourceIds.map { BulkStatementResponse(it.value, statementController.findByResource(it, page, items, by, sort).body!!) })
+        return ok(resourceIds.map { BulkGetStatementsResponse(it.value, statementController.findByResource(it, page, items, by, sort).body!!) })
     }
 
     @GetMapping("/objects")
     fun findByObjects(
         @RequestParam("ids") resourceIds: List<ResourceId>
-    ): ResponseEntity<Iterable<BulkStatementResponse>> {
+    ): ResponseEntity<Iterable<BulkGetStatementsResponse>> {
         val page = 1
         val items = 99999
         val by = "id"
         val sort = false
-        return ok(resourceIds.map { BulkStatementResponse(it.value, statementController.findByObject(it.value, page, items, by, sort).body!!) })
+        return ok(resourceIds.map { BulkGetStatementsResponse(it.value, statementController.findByObject(it.value, page, items, by, sort).body!!) })
     }
 
     @DeleteMapping("/")
@@ -49,10 +52,23 @@ class BulkStatementController(
     ) {
         statementsIds.forEach { statementController.delete(it) }
     }
+
+    @PutMapping("/")
+    fun edit(
+        @RequestParam("ids") statementsIds: List<StatementId>,
+        @RequestBody(required = true) statementEditRequest: StatementEditRequest
+    ): ResponseEntity<Iterable<BulkPutStatementResponse>> {
+        return ok(statementsIds.map { BulkPutStatementResponse(it, statementController.edit(it, statementEditRequest).body!!) })
+    }
 }
 
-data class BulkStatementResponse(
-    @JsonProperty("resource_id")
-    val resourceId: String,
+data class BulkGetStatementsResponse(
+    val id: String,
     val statements: Iterable<StatementResponse>
+)
+
+data class BulkPutStatementResponse(
+    @JsonProperty("id")
+    val statementId: StatementId,
+    val statement: StatementResponse
 )

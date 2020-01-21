50b1b2806ecac368b859566b03cf44552fd663b8
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-01-21 14:50:59 +0100

    Custom query to get papers count per field

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jStatsRepository.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jStatsRepository.kt
@@ -1,6 +1,7 @@
 package eu.tib.orkg.prototype.statements.domain.model.neo4j
 
 import org.springframework.data.neo4j.annotation.Query
+import org.springframework.data.neo4j.annotation.QueryResult
 import org.springframework.data.neo4j.repository.Neo4jRepository
 
 interface Neo4jStatsRepository : Neo4jRepository<Neo4jResource, Long> {
@@ -10,4 +11,14 @@ interface Neo4jStatsRepository : Neo4jRepository<Neo4jResource, Long> {
 
     @Query("""MATCH (n:Resource{resource_id: 'R11'}) CALL apoc.path.subgraphAll(n, {relationshipFilter: '>'}) YIELD nodes UNWIND nodes as field WITH COUNT(field) as cnt RETURN cnt""")
     fun getResearchFieldsCount(): Long
+
+    @Query("""MATCH (n:ResearchField) WITH n OPTIONAL MATCH (n)-[:RELATES_TO*0..3 {predicate_id: 'P36'}]->(:ResearchField)<-[:RELATES_TO {predicate_id: 'P30'}]-(p:Paper) RETURN n.resource_id AS fieldId, n.label AS field, COUNT(p) AS papers""")
+    fun getResearchFieldsPapersCount(): Iterable<FieldsStats>
 }
+
+@QueryResult
+data class FieldsStats(
+    val fieldId: String,
+    val field: String,
+    val papers: Long
+)

1082d8678a1852af7c5c21734d198a367b6259d0
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-01-31 12:23:00 +0100

    Remove unused listByIds from statement repo

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jStatementRepository.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jStatementRepository.kt
@@ -27,22 +27,4 @@ interface Neo4jStatementRepository :
 
     @Query("""MATCH (p:`Thing`})-[*]->() WHERE p.`resource_id`={0} OR p.`literal_id`={0} OR p.`predicate_id`={0} OR p.`class_id`={0} RETURN COUNT(p)""")
     fun countByIdRecursive(paperId: String): Int
-
-    @Query(
-        "MATCH (s:Thing)-[p:RELATED]->(o:Thing)\n" +
-            "WITH CASE \n" +
-            " WHEN 'Resource' IN LABELS(s) THEN s.resource_id\n" +
-            " WHEN 'Literal' IN LABELS(s) THEN s.literal_id\n" +
-            " WHEN 'Predicate' IN LABELS(s) THEN s.predicate_id\n" +
-            " ELSE s.class_id\n" +
-            "END AS subjectId,\n" +
-            "CASE \n" +
-            " WHEN 'Resource' IN LABELS(o) THEN o.resource_id\n" +
-            " WHEN 'Literal' IN LABELS(o) THEN o.literal_id\n" +
-            " WHEN 'Predicate' IN LABELS(o) THEN o.predicate_id\n" +
-            " ELSE o.class_id\n" +
-            "END AS objectId, p.predicate_id AS predicateId\n" +
-            "RETURN subjectId, predicateId, objectId"
-    )
-    fun listByIds(): List<Triple<String, PredicateId, String>>
 }

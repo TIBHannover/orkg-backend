4915aa84946268b5fada6527097e02cf20f82f35
yaser <yaser.jaradeh@gmail.com>
2020-03-23 17:14:15 +0100

    Use val instead of var in regex utils

--- src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/utils.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/utils.kt
@@ -2,7 +2,7 @@ package eu.tib.orkg.prototype.statements.infrastructure.neo4j
 
 import java.util.regex.Pattern
 
-var regexCharacters = Pattern.compile("""[<(\[{\\^\-=${'$'}!|\]})?*+.>]""").toRegex()
+val regexCharacters = Pattern.compile("""[<(\[{\\^\-=${'$'}!|\]})?*+.>]""").toRegex()
 
 fun escapeRegexString(str: String) =
     regexCharacters.replace(str) {

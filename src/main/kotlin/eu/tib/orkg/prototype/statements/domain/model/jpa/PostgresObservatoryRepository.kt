4fd361d1da40174797e5d77dceb61709cd24f4f6
Muhammad Haris <Muhammad.Haris@tib.eu>
2020-04-02 13:37:01 +0200

    Added functionality list

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/jpa/PostgresObservatoryRepository.kt
+++
@@ -1,6 +0,0 @@
-package eu.tib.orkg.prototype.statements.domain.model.jpa
-
-import java.util.UUID
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PostgresObservatoryRepository : JpaRepository<OrganizationEntity, UUID>

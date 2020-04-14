ec09999cf6615f3f385d990c42973fb23ad04d01
Manuel Prinz <manuel.prinz@tib.eu>
2020-04-14 12:53:32 +0200

    Create custom exceptions for validation

--- src/main/kotlin/eu/tib/orkg/prototype/core/rest/ExceptionHandler.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/core/rest/ExceptionHandler.kt
@@ -1,12 +1,15 @@
 package eu.tib.orkg.prototype.core.rest
 
+import eu.tib.orkg.prototype.statements.application.PropertyValidationException
 import eu.tib.orkg.prototype.toSnakeCase
 import java.time.OffsetDateTime
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
@@ -27,6 +30,23 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
         val payload = ValidationFailureResponse(
             status = status.value(),
             error = status.reasonPhrase,
+            errors = errors
+        )
+        return ResponseEntity.badRequest().body(payload)
+    }
+
+    @ExceptionHandler(PropertyValidationException::class)
+    fun handleIllegalArgumentException(ex: PropertyValidationException): ResponseEntity<Any> {
+        // Use a list so that it is compatible to the javax.validation responses
+        val errors = listOf(
+            FieldValidationError(
+                field = ex.property,
+                message = ex.message
+            )
+        )
+        val payload = ValidationFailureResponse(
+            status = BAD_REQUEST.value(),
+            error = BAD_REQUEST.reasonPhrase,
             errors = errors
         )
         return ResponseEntity.badRequest().body(payload)

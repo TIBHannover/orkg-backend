acbe855d695df4d0ff95efe21fa8c255d0f3c8f4
Muhammad Haris <mharis,msit15seecs@seecs.edu.pk>
2020-05-22 10:59:19 +0200

    Reformatted OrganizationCotnroller

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/OrganizationController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/OrganizationController.kt
@@ -78,8 +78,8 @@ class OrganizationController(
 
     fun decoder(base64Str: String, name: UUID?) {
         val (mimeType, encodedString) = base64Str.split(",")
-        //var a: String = "data:image/svg;base64"
-        val (extension, data ) = (mimeType.substring(mimeType
+        // var a: String = "data:image/svg;base64"
+        val (extension, data) = (mimeType.substring(mimeType
                                 .indexOf("/") + 1))
                                 .split(";")
 

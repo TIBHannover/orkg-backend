c9d5c1a5a49d7ef1fc326e6a2d22e91a8b1ef408
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-06-16 12:11:44 +0200

    Check if constraint exists otherwise create it

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jIndexRepository.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jIndexRepository.kt
@@ -1,18 +1,41 @@
 package eu.tib.orkg.prototype.statements.domain.model.neo4j
 
+import org.neo4j.ogm.session.Session
 import org.springframework.data.neo4j.annotation.Query
+import org.springframework.data.neo4j.annotation.QueryResult
 import org.springframework.data.neo4j.repository.Neo4jRepository
 
-interface Neo4jIndexRepository : Neo4jRepository<Any, Long> {
+interface Neo4jIndexRepository : Neo4jRepository<Neo4jResource, Long>, IndexRepository {
 
-    // TODO: in Neo4j 3.5 you can't set the name, you can in Neo4j 4.0
-    @Query("""CREATE CONSTRAINT ON (n:{0}) ASSERT n.{1} IS UNIQUE;""")
+    @Query("""CALL db.indexes() YIELD tokenNames, properties, type RETURN tokenNames[0] as label, properties[0] as property, type as type""")
+    fun getExistingIndicesAndConstraints(): Iterable<Neo4jIndexInfo>
+}
+
+interface IndexRepository {
+
     fun createUniqueConstraint(type: String, property: String)
 
-    // TODO: in Neo4j 3.5 you can't set the name and the create index statement changes in Neo4j 4.0
-    @Query("""CREATE INDEX ON :{0}({1});""")
     fun createPropertyIndex(type: String, property: String)
-
-    @Query("""CALL db.indexes() YIELD tokenNames, properties, type RETURN tokenNames[0] as label, properties[0] as property, type""")
-    fun getExistingIndicesAndConstraints(): Triple<List<String>, List<String>, String>
 }
+
+class IndexRepositoryImpl(private val ogmSession: Session) : IndexRepository {
+
+    // TODO: in Neo4j 3.5 you can't set the name and the create index statement changes in Neo4j 4.0
+    override fun createPropertyIndex(type: String, property: String) {
+        val query = """CREATE INDEX ON :$type($property);"""
+        ogmSession.query(query, emptyMap<String, Any>())
+    }
+
+    // TODO: in Neo4j 3.5 you can't set the name, you can in Neo4j 4.0
+    override fun createUniqueConstraint(type: String, property: String) {
+        val query = """CREATE CONSTRAINT ON (n:$type) ASSERT n.$property IS UNIQUE;"""
+        ogmSession.query(query, emptyMap<String, Any>())
+    }
+}
+
+@QueryResult
+data class Neo4jIndexInfo(
+    val label: String,
+    val property: String,
+    val type: String
+)
--- src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jIndexService.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jIndexService.kt
@@ -1,6 +1,7 @@
 package eu.tib.orkg.prototype.statements.infrastructure.neo4j
 
 import eu.tib.orkg.prototype.statements.domain.model.IndexService
+import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jIndexInfo
 import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jIndexRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,20 +13,43 @@ class Neo4jIndexService(
 ) : IndexService {
 
     override fun createRequiredUniqueConstraints() {
-        neo4jIndexRepository.createUniqueConstraint( "Class", "class_id")
-        neo4jIndexRepository.createUniqueConstraint( "Literal", "literal_id")
-        neo4jIndexRepository.createUniqueConstraint( "Predicate", "predicate_id")
-        neo4jIndexRepository.createUniqueConstraint("Resource", "resource_id")
-        neo4jIndexRepository.createUniqueConstraint( "Thing", "class_id")
-        neo4jIndexRepository.createUniqueConstraint("Thing", "literal_id")
-        neo4jIndexRepository.createUniqueConstraint("Thing", "predicate_id")
-        neo4jIndexRepository.createUniqueConstraint("Thing", "resource_id")
+        val existingConstraints = neo4jIndexRepository.getExistingIndicesAndConstraints()
+        checkAndCreateConstraint(existingConstraints, "Class", "class_id", IndexType.UNIQUE)
+        checkAndCreateConstraint(existingConstraints, "Literal", "literal_id", IndexType.UNIQUE)
+        checkAndCreateConstraint(existingConstraints, "Predicate", "predicate_id", IndexType.UNIQUE)
+        checkAndCreateConstraint(existingConstraints, "Resource", "resource_id", IndexType.UNIQUE)
+        checkAndCreateConstraint(existingConstraints, "Thing", "class_id", IndexType.UNIQUE)
+        checkAndCreateConstraint(existingConstraints, "Thing", "literal_id", IndexType.UNIQUE)
+        checkAndCreateConstraint(existingConstraints, "Thing", "predicate_id", IndexType.UNIQUE)
+        checkAndCreateConstraint(existingConstraints, "Thing", "resource_id", IndexType.UNIQUE)
     }
 
     override fun createRequiredPropertyIndices() {
-        neo4jIndexRepository.createPropertyIndex( "Literal", "label")
-        /*neo4jIndexRepository.createPropertyIndex("Predicate", "label")
-        neo4jIndexRepository.createPropertyIndex( "Resource", "label")
-        neo4jIndexRepository.createPropertyIndex("Class", "label")*/
+        val existingConstraints = neo4jIndexRepository.getExistingIndicesAndConstraints()
+        checkAndCreateConstraint(existingConstraints, "Literal", "label", IndexType.PROPERTY)
+        checkAndCreateConstraint(existingConstraints, "Predicate", "label", IndexType.PROPERTY)
+        checkAndCreateConstraint(existingConstraints, "Resource", "label", IndexType.PROPERTY)
+        checkAndCreateConstraint(existingConstraints, "Class", "label", IndexType.PROPERTY)
     }
+
+    private fun checkAndCreateConstraint(
+        existingConstraints: Iterable<Neo4jIndexInfo>,
+        label: String,
+        property: String,
+        indexType: IndexType
+    ) {
+        val found = existingConstraints
+            .firstOrNull { it.label == label && it.property == property && it.type == indexType.value }
+        if (found == null) {
+            if (indexType == IndexType.PROPERTY)
+                neo4jIndexRepository.createPropertyIndex(label, property)
+            else
+                neo4jIndexRepository.createUniqueConstraint(label, property)
+        }
+    }
+}
+
+private enum class IndexType(val value: String) {
+    UNIQUE("node_unique_property"),
+    PROPERTY("node_label_property")
 }

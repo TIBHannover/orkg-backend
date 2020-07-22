896d77dd634b9b084fcaa03d3eb9dbe83e467abd
Manuel Prinz <manuel.prinz@tib.eu>
2020-07-22 18:40:48 +0200

    Move contributor functionality to separate components

---
+++ src/main/kotlin/eu/tib/orkg/prototype/contributions/domain/model/ContributorService.kt
@@ -0,0 +1,34 @@
+package eu.tib.orkg.prototype.contributions.domain.model
+
+import eu.tib.orkg.prototype.auth.persistence.UserEntity
+import eu.tib.orkg.prototype.auth.service.UserRepository
+import java.util.Optional
+import java.util.UUID
+import javax.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+@Transactional
+class ContributorService(
+    private val userRepository: UserRepository
+) {
+    fun findById(userId: UUID): Optional<Contributor> =
+        userRepository
+            .findOrganizationById(userId)
+            .map(UserEntity::toContributor)
+
+    fun findOrganizationById(userId: UUID): Optional<Contributor> =
+        userRepository
+            .findOrganizationById(userId)
+            .map(UserEntity::toContributor)
+
+    fun findUsersByOrganizationId(id: UUID): Iterable<Contributor> =
+        userRepository
+            .findUsersByOrganizationId(id)
+            .map(UserEntity::toContributor)
+
+    fun findUsersByObservatoryId(id: UUID): Iterable<Contributor> =
+        userRepository
+            .findUsersByObservatoryId(id)
+            .map(UserEntity::toContributor)
+}

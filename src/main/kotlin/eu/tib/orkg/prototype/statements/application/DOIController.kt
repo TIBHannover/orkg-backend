89076348dc36bcf11cd5d719bd61981666943ba7
mharis111 <38453478+mharis111@users.noreply.github.com>
2020-07-28 16:56:59 +0200

    Update DOIController.kt

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/DOIController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/DOIController.kt
@@ -3,7 +3,6 @@ package eu.tib.orkg.prototype.statements.application
 import eu.tib.orkg.prototype.auth.service.UserService
 import eu.tib.orkg.prototype.statements.domain.model.Literal
 import eu.tib.orkg.prototype.statements.domain.model.LiteralService
-import eu.tib.orkg.prototype.statements.domain.model.Object
 import eu.tib.orkg.prototype.statements.domain.model.ObservatoryService
 import eu.tib.orkg.prototype.statements.domain.model.OrganizationService
 import eu.tib.orkg.prototype.statements.domain.model.PredicateId
@@ -12,8 +11,7 @@ import eu.tib.orkg.prototype.statements.domain.model.ResourceId
 import eu.tib.orkg.prototype.statements.domain.model.ResourceService
 import eu.tib.orkg.prototype.statements.domain.model.StatementService
 import eu.tib.orkg.prototype.statements.domain.model.Thing
-import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jLiteral
-import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jResource
+import java.time.LocalDate
 import java.util.Base64
 import java.util.UUID
 import org.springframework.beans.factory.annotation.Value
@@ -23,7 +21,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/dois/")
@@ -47,43 +44,38 @@ class DOIController(
 
     @PostMapping("/")
     fun addDOI(@RequestBody doiData: CreateDOIRequest): String? {
-
-        //var a = "$dataciteTestUsername:$dataciteTestPassword"
-        //createXmlMetadata(doiData.comparisonId, doiData.description, doiData.title, doiData.subject)
         val base64 = Base64.getEncoder().encodeToString(("$dataciteTestUsername:$dataciteTestPassword").toByteArray())
         getCreatorsXml(doiData.authors)
         getRelatedPapers(doiData.relatedResources)
-        //doiData.authors.forEach { println(it.creator +" "+ it.ORCID) }
         return createXmlMetadata(doiData.comparisonId, doiData.description, doiData.title, getCreatorsXml(doiData.authors), getRelatedPapers(doiData.relatedResources), doiData.subject)
-       //return base64
     }
 
     fun createXmlMetadata(comparisonId: String, description: String, title: String, creators: String, relatedIdentifiers: String, subject: String): String {
-        var xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
-        "<resource xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://datacite.org/schema/kernel-4\" xsi:schemaLocation=\"http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4.3/metadata.xsd\">\n"+
-        "<identifier identifierType=\"DOI\">${dataciteTestDOIPrefix}/${comparisonId}</identifier>\n"+
-        "<creators>\n"+
-            "${creators}\n"+
-        "</creators>\n"+
-        "<titles>\n"+
-            "<title xml:lang=\"en\">${title}</title>\n"+
-        "</titles>\n"+
-        "<publisher xml:lang=\"en\">Open Research Knowledge Graph</publisher>\n"+
-        "<publicationYear>${LocalDate.now().year}</publicationYear>\n"+
-        "<subjects>\n"+
-            "<subject xml:lang=\"en\">${subject}</subject>\n"+
-        "</subjects>\n"+
-        "<language>en</language>\n"+
-        "<resourceType resourceTypeGeneral=\"Dataset\">Comparison</resourceType>\n"+
-        "<relatedIdentifiers>\n"+
-        "${relatedIdentifiers}\n"+
-        "</relatedIdentifiers>\n"+
-        "<rightsList>\n"+
-        "<rights rightsURI=\"https://creativecommons.org/licenses/by-sa/4.0/\">Creative Commons Attribution-ShareAlike 4.0 International License.</rights>\n"+
-        "</rightsList>\n"+
-        "<descriptions>\n"+
-        "<description descriptionType=\"Abstract\">${description}</description>\n"+
-        "</descriptions>\n"+
+        var xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+        "<resource xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://datacite.org/schema/kernel-4\" xsi:schemaLocation=\"http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4.3/metadata.xsd\">\n" +
+        "<identifier identifierType=\"DOI\">$dataciteTestDOIPrefix/$comparisonId</identifier>\n" +
+        "<creators>\n" +
+            "${creators}\n" +
+        "</creators>\n" +
+        "<titles>\n" +
+            "<title xml:lang=\"en\">$title</title>\n" +
+        "</titles>\n" +
+        "<publisher xml:lang=\"en\">Open Research Knowledge Graph</publisher>\n" +
+        "<publicationYear>${LocalDate.now().year}</publicationYear>\n" +
+        "<subjects>\n" +
+            "<subject xml:lang=\"en\">$subject</subject>\n" +
+        "</subjects>\n" +
+        "<language>en</language>\n" +
+        "<resourceType resourceTypeGeneral=\"Dataset\">Comparison</resourceType>\n" +
+        "<relatedIdentifiers>\n" +
+            "${relatedIdentifiers}\n" +
+        "</relatedIdentifiers>\n" +
+        "<rightsList>\n" +
+        "<rights rightsURI=\"https://creativecommons.org/licenses/by-sa/4.0/\">Creative Commons Attribution-ShareAlike 4.0 International License.</rights>\n" +
+        "</rightsList>\n" +
+        "<descriptions>\n" +
+        "<description descriptionType=\"Abstract\">$description</description>\n" +
+        "</descriptions>\n" +
         "</resource>\n"
         return xml
     }
@@ -91,46 +83,38 @@ class DOIController(
     fun getCreatorsXml(authors: Set<Creator>): String {
         var authorsList = ""
         authors.forEach {
-            authorsList+="<creator>\n"+
-            "<creatorName nameType=\"Personal\">${it.creator}</creatorName>\n"+
-            "<nameIdentifier schemeURI=\"http://orcid.org/\" nameIdentifierScheme=\"ORCID\">${it.ORCID}</nameIdentifier>\n"+
+            authorsList += "<creator>\n" +
+            "<creatorName nameType=\"Personal\">${it.creator}</creatorName>\n" +
+            "<nameIdentifier schemeURI=\"http://orcid.org/\" nameIdentifierScheme=\"ORCID\">${it.ORCID}</nameIdentifier>\n" +
             "</creator\n"
-            //println(it.creator+" "+it.ORCID)
         }
         return authorsList
-        //authors.forEach { println(it.creator+" "+it.ORCID) }
     }
 
     fun getRelatedPapers(relatedResources: Set<ResourceId>): String {
         val pagination = PageRequest.of(0, 1)
-        var relatedIdentifiers=""
-        //var s = statementService.findAllBySubject(, pagination)
-        //var result = null
+        var relatedIdentifiers = ""
         relatedResources.forEach {
             var statementResult = statementService.findAllByObject(it.value, pagination)
             for (statement in statementResult) {
-                var paper = refreshObject(statement.subject)
+                var paper = refreshSubject(statement.subject)
                 var result = statementService.findAllBySubjectAndPredicate(paper.id.toString(), PredicateId(ID_DOI_PREDICATE), pagination)
-                for(res in result) {
-                    //println(refreshObject1(res.`object`).label)
-                    relatedIdentifiers+="<relatedIdentifier relationType=\"IsDerivedFrom\" relatedIdentifierType=\"DOI\">${refreshObject1(res.`object`).label}</relatedIdentifier>\n"
+                for (res in result) {
+                    relatedIdentifiers += "<relatedIdentifier relationType=\"IsDerivedFrom\" relatedIdentifierType=\"DOI\">${refreshObject(res.`object`).label}</relatedIdentifier>\n"
                 }
-                //(result.forEach { it-> println(it.id) })
             }
-            //result.forEach { it -> println(it.subject) }
-            //println(it.value)
         }
         return relatedIdentifiers
     }
 
-    private fun refreshObject(thing: Thing): Resource {
+    private fun refreshSubject(thing: Thing): Resource {
         return when (thing) {
             is Resource -> thing
             else -> error("")
         }
     }
 
-    private fun refreshObject1(thing: Thing): Literal {
+    private fun refreshObject(thing: Thing): Literal {
         return when (thing) {
             is Literal -> thing
             else -> error("")

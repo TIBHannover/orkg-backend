544c34059220aba7ac3ce35889390d0b69a11728
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-08-12 11:02:45 +0000

    Get top (n) research problems for the homepage
    (cherry picked from commit 79c8b59fc7016bca19d823f09b1ab08643b88e5f)

--- src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jProblemService.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jProblemService.kt
@@ -1,8 +1,10 @@
 package eu.tib.orkg.prototype.statements.infrastructure.neo4j
 
 import eu.tib.orkg.prototype.statements.domain.model.ProblemService
+import eu.tib.orkg.prototype.statements.domain.model.Resource
 import eu.tib.orkg.prototype.statements.domain.model.ResourceId
 import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jProblemRepository
+import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jResource
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -19,5 +21,22 @@ class Neo4jProblemService(
                 val freq = it.freq
             }
         }
+    }
+
+    override fun getTopResearchProblems(): List<Resource> =
+        getTopResearchProblemsGoingBack(listOf(1, 2, 3, 6))
+            .map(Neo4jResource::toResource)
+
+    /*
+    Iterate over the list of months, and if no problems are found go back a bit more in time
+    and if none found take all time results
+     */
+    private fun getTopResearchProblemsGoingBack(listOfMonths: List<Int>): Iterable<Neo4jResource> {
+        val month = listOfMonths.firstOrNull() ?: return neo4jProblemRepository.getTopResearchProblemsAllTime()
+        val problems = neo4jProblemRepository.getTopResearchProblemsGoingBack(month)
+        return if (problems.count() > 0)
+            problems
+        else
+            getTopResearchProblemsGoingBack(listOfMonths.drop(1))
     }
 }

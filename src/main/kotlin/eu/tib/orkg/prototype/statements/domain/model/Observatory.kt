0365b6d9def1c5a656358910b98471e78198e7af
mharis111 <38453478+mharis111@users.noreply.github.com>
2020-09-18 12:27:31 +0200

    Update Observatory.kt

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/Observatory.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/Observatory.kt
@@ -1,5 +1,6 @@
 package eu.tib.orkg.prototype.statements.domain.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import eu.tib.orkg.prototype.auth.persistence.UserEntity
 import eu.tib.orkg.prototype.statements.domain.model.jpa.OrganizationEntity
 import java.util.UUID
@@ -8,6 +9,7 @@ data class Observatory(
     val id: UUID?,
     var name: String?,
     var description: String?,
+    @JsonProperty("research_field")
     var researchField: String?,
     val users: Set<UserEntity>?,
     val organizations: Set<OrganizationEntity>?,

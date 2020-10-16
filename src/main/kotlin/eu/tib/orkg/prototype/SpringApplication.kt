cac876a0475c36430f367f3808038c84f0b5a9fc
Manuel Prinz <manuel.prinz@tib.eu>
2020-10-16 16:49:38 +0200

    Remove obsolete code
    The configuration of CORS origins and the respective tests are now
    obsolete and can be deleted.

--- src/main/kotlin/eu/tib/orkg/prototype/SpringApplication.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/SpringApplication.kt
@@ -1,6 +1,5 @@
 package eu.tib.orkg.prototype
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
@@ -10,9 +9,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 class Application : SpringBootServletInitializer() {
     override fun configure(builder: SpringApplicationBuilder?) =
         builder?.sources(Application::class.java)
-
-    @Value("\${orkg.cors.origins:*}")
-    val origins: List<String>? = null
 }
 
 fun main(args: Array<String>) {

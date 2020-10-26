73a78a8459c0f72342eb2833caba0d71d520aa36
Manuel Prinz <manuel.prinz@tib.eu>
2020-10-26 15:20:29 +0100

    Add JSON serialization for ContributorIds

--- src/main/kotlin/eu/tib/orkg/prototype/contributions/domain/model/ContributorId.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/contributions/domain/model/ContributorId.kt
@@ -1,9 +1,17 @@
 package eu.tib.orkg.prototype.contributions.domain.model
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import eu.tib.orkg.prototype.contributions.application.json.ContributorIdDeserializer
+import eu.tib.orkg.prototype.contributions.application.json.ContributorIdSerializer
 import java.util.UUID
 
+@JsonDeserialize(using = ContributorIdDeserializer::class)
+@JsonSerialize(using = ContributorIdSerializer::class)
 data class ContributorId(val value: UUID) {
     companion object {
         fun createUnknownContributor() = ContributorId(UUID(0, 0))
     }
+
+    override fun toString() = value.toString()
 }

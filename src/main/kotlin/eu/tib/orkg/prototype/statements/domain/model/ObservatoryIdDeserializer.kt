2db7cb65f799c767cc2378979a8ff70fc2eaa84a
Golsa Heidari <golsa.heidary@gamil.com>
2020-11-05 16:13:59 +0100

    Introduce ObservatoryId

---
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/ObservatoryIdDeserializer.kt
@@ -0,0 +1,23 @@
+package eu.tib.orkg.prototype.statements.domain.model
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import eu.tib.orkg.prototype.statements.application.InvalidUUID
+import java.util.UUID
+
+class ObservatoryIdDeserializer :
+    JsonDeserializer<ObservatoryId>() {
+
+    override fun deserialize(
+        p: JsonParser?,
+        ctxt: DeserializationContext?
+    ): ObservatoryId? =
+        p?.valueAsString?.let {
+            try {
+                ObservatoryId(UUID.fromString(it))
+            } catch (exception: IllegalArgumentException) {
+                throw InvalidUUID(it, exception)
+            }
+        }
+}

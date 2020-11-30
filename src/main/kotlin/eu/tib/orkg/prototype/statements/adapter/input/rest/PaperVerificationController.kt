e21ed8e60415f548fa34107b229e87d82f17415a
Manuel Prinz <manuel.prinz@tib.eu>
2020-11-30 16:23:02 +0100

    Use old model of pagination
    This is required to be compatible with the current way of querying.

--- src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/input/rest/PaperVerificationController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/input/rest/PaperVerificationController.kt
@@ -1,12 +1,13 @@
 package eu.tib.orkg.prototype.statements.adapter.input.rest
 
+import eu.tib.orkg.prototype.createPageable
 import eu.tib.orkg.prototype.statements.application.BaseController
 import eu.tib.orkg.prototype.statements.application.port.out.LoadPaperPort
 import eu.tib.orkg.prototype.statements.domain.model.Resource
 import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,10 +17,28 @@ class PaperVerificationController(
 ) : BaseController() {
 
     @GetMapping("/Paper/resources/", params = ["verified=true"])
-    fun loadVerifiedPapers(pageable: Pageable): Page<Resource> =
-        adapter.loadVerifiedPapers(pageable)
+    fun loadVerifiedPapers(
+        @RequestParam("page", required = false) page: Int?,
+        @RequestParam("items", required = false) items: Int?,
+        @RequestParam("sortBy", required = false) sortBy: String?,
+        @RequestParam("desc", required = false, defaultValue = "false") desc: Boolean,
+        @RequestParam("q", required = false) searchString: String?,
+        @RequestParam("exact", required = false, defaultValue = "false") exactMatch: Boolean
+    ): Page<Resource> {
+        val pageable = createPageable(page, items, sortBy, desc)
+        return adapter.loadVerifiedPapers(pageable)
+    }
 
     @GetMapping("/Paper/resources/", params = ["verified=false"])
-    fun loadUnverifiedPapers(pageable: Pageable): Page<Resource> =
-        adapter.loadUnverifiedPapers(pageable)
+    fun loadUnverifiedPapers(
+        @RequestParam("page", required = false) page: Int?,
+        @RequestParam("items", required = false) items: Int?,
+        @RequestParam("sortBy", required = false) sortBy: String?,
+        @RequestParam("desc", required = false, defaultValue = "false") desc: Boolean,
+        @RequestParam("q", required = false) searchString: String?,
+        @RequestParam("exact", required = false, defaultValue = "false") exactMatch: Boolean
+    ): Page<Resource> {
+        val pageable = createPageable(page, items, sortBy, desc)
+        return adapter.loadUnverifiedPapers(pageable)
+    }
 }

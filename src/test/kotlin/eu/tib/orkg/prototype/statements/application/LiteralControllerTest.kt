433a73002e21338dcf5426e897b531c6398d45dc
Manuel Prinz <manuel.prinz@tib.eu>
2020-12-08 11:35:28 +0100

    Allow literals to be empty

--- src/test/kotlin/eu/tib/orkg/prototype/statements/application/LiteralControllerTest.kt
+++ src/test/kotlin/eu/tib/orkg/prototype/statements/application/LiteralControllerTest.kt
@@ -55,30 +55,43 @@ class LiteralControllerTest {
     }
 
     @Test
-    fun whenPOST_AndLabelIsBlank_ThenFailValidation() {
-        val literal = createCreateRequestWithBlankLabel()
+    @WithMockUser(username = "f2d66c90-3cbf-4d4f-951f-0fc470f682c4")
+    fun whenPOST_AndLabelIsEmptyString_ThenSucceed() {
+        val literal = createCreateRequestWithEmptyLabel()
+        val mockResult = Literal(
+            id = LiteralId(1),
+            label = literal.label,
+            datatype = literal.datatype,
+            createdAt = OffsetDateTime.now()
+        )
+        every { literalService.findById(any()) } returns Optional.of(mockResult)
+        every { literalService.create(any(), any(), any()) } returns mockResult
 
         mockMvc
             .perform(creationOf(literal))
-            .andExpect(status().isBadRequest)
-            .andExpect(jsonPath("\$.status").value(400))
-            .andExpect(jsonPath("\$.errors.length()").value(1))
-            .andExpect(jsonPath("\$.errors[0].field").value("label"))
-            .andExpect(jsonPath("\$.errors[0].message").value("must not be blank"))
+            .andExpect(status().isCreated)
+            .andExpect(header().string("Location", "http://localhost/api/literals/L1"))
+
+        verify(exactly = 1) {
+            literalService.create(ContributorId("f2d66c90-3cbf-4d4f-951f-0fc470f682c4"), "", "xsd:string")
+        }
     }
 
     @Test
-    fun whenPUT_AndLabelIsBlank_ThenFailValidation() {
-        val literal = createUpdateRequestWithBlankLabel()
-        every { literalService.findById(any()) } returns Optional.of(createDummyLiteral())
+    fun whenPUT_AndLabelIsEmpty_ThenSucceed() {
+        val literal = createUpdateRequestWithEmptyLabel()
+        val double = createDummyLiteral() // needed so "expected" has the the same timestamp
+        val expected = double.copy(label = "")
+        every { literalService.findById(any()) } returns Optional.of(double)
+        every { literalService.update(any()) } returns expected
 
         mockMvc
             .perform(updateOf(literal, "L1"))
-            .andExpect(status().isBadRequest)
-            .andExpect(jsonPath("\$.status").value(400))
-            .andExpect(jsonPath("\$.errors.length()").value(1))
-            .andExpect(jsonPath("\$.errors[0].field").value("label"))
-            .andExpect(jsonPath("\$.errors[0].message").value("must not be blank"))
+            .andExpect(status().isOk)
+
+        verify(exactly = 1) {
+            literalService.update(expected)
+        }
     }
 
     @Test
@@ -151,8 +164,13 @@ class LiteralControllerTest {
 
     private fun createCreateRequestWithBlankLabel() = LiteralCreateRequest(label = " ".repeat(5))
 
+    private fun createCreateRequestWithEmptyLabel() = LiteralCreateRequest(label = "")
+
     private fun createUpdateRequestWithBlankLabel() =
         LiteralUpdateRequest(id = null, label = " ".repeat(5), datatype = null)
+
+    private fun createUpdateRequestWithEmptyLabel() =
+        LiteralUpdateRequest(id = null, label = "", datatype = null)
 
     private fun createUpdateRequestWithBlankDatatype() =
         LiteralUpdateRequest(id = null, label = null, datatype = " ".repeat(5))

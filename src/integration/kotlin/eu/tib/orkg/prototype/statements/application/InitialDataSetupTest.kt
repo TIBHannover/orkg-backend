5a68bce226dfc239227bc19a430d8bdc77f79f25
Suhas Murthy <suhas.murthy@tib.eu>
2020-12-15 14:46:25 +0100

    Fixed build errors

--- src/integration/kotlin/eu/tib/orkg/prototype/statements/application/InitialDataSetupTest.kt
+++ src/integration/kotlin/eu/tib/orkg/prototype/statements/application/InitialDataSetupTest.kt
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
-import org.slf4j.LoggerFactory
 
 @DisplayName("Initial Data Setup")
 class InitialDataSetupTest {

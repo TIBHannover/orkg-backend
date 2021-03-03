ed4cb051f55c6e6855a0cb0268930ea9ee24c55a
mharis111 <38453478+mharis111@users.noreply.github.com>
2021-03-03 14:43:55 +0100

    Handling case in-sensitiveness for named URLs

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/jpa/PostgresOrganizationRepository.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/jpa/PostgresOrganizationRepository.kt
@@ -3,9 +3,11 @@ package eu.tib.orkg.prototype.statements.domain.model.jpa
 import java.util.Optional
 import java.util.UUID
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface PostgresOrganizationRepository : JpaRepository<OrganizationEntity, UUID> {
 
+    @Query("""FROM OrganizationEntity WHERE (displayId=:name or displayId=LOWER(:name))""")
     fun findByDisplayId(name: String): Optional<OrganizationEntity>
 
     fun findByName(name: String): Optional<OrganizationEntity>
--- src/main/resources/db/changelog/changes/0010-add-url-column-in-organization.sql
+++ src/main/resources/db/changelog/changes/0010-add-url-column-in-organization.sql
@@ -8,7 +8,7 @@ alter table organizations
     add display_id varchar not null default 'randomText';
 
 --replacing default value with name of organization, also replacing spaces and commas with dashes ( - )
-update organizations set display_id=replace(replace(organizations.name,' ','-'), '''','');
+update organizations set display_id=LOWER(replace(replace(organizations.name,' ','-'), '''',''));
 
 
 create unique index organizations_display_id_uindex on organizations (display_id);

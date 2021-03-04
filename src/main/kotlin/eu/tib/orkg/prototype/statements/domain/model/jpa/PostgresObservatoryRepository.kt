6b02fb7d71c9a3df22db72b578f225e414e082ea
mharis111 <38453478+mharis111@users.noreply.github.com>
2021-03-04 09:53:39 +0100

    Handling case in-sensitiveness for displayId

--- src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/jpa/PostgresObservatoryRepository.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/jpa/PostgresObservatoryRepository.kt
@@ -2,6 +2,7 @@ package eu.tib.orkg.prototype.statements.domain.model.jpa
 import java.util.Optional
 import java.util.UUID
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface PostgresObservatoryRepository : JpaRepository<ObservatoryEntity, UUID> {
 
@@ -9,6 +10,7 @@ interface PostgresObservatoryRepository : JpaRepository<ObservatoryEntity, UUID>
 
     fun findByName(name: String): Optional<ObservatoryEntity>
 
+    @Query("""FROM ObservatoryEntity WHERE (displayId=:name or displayId=LOWER(:name))""")
     fun findByDisplayId(name: String): Optional<ObservatoryEntity>
 
     fun findByResearchField(researchField: String): List<ObservatoryEntity>

30c11de6558fdb9d1b4951baef26db4258963c3f
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2021-03-15 17:58:13 +0100

    Use new pagination in controller

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/ProblemController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/ProblemController.kt
@@ -1,10 +1,11 @@
 package eu.tib.orkg.prototype.statements.application
 
+import eu.tib.orkg.prototype.contributions.domain.model.ContributorId
 import eu.tib.orkg.prototype.contributions.domain.model.ContributorService
-import eu.tib.orkg.prototype.createPageable
 import eu.tib.orkg.prototype.statements.domain.model.ProblemService
 import eu.tib.orkg.prototype.statements.domain.model.Resource
 import eu.tib.orkg.prototype.statements.domain.model.ResourceId
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -32,12 +33,10 @@ class ProblemController(
     @GetMapping("/{problemId}/users")
     fun getContributorsPerProblem(
         @PathVariable problemId: ResourceId,
-        @RequestParam("page", required = false) page: Int?,
-        @RequestParam("items", required = false) items: Int?
+        pageable: Pageable
     ): ResponseEntity<Iterable<Any>> {
-        val pagination = createPageable(page, items, null, false)
-        val contributors = service.findContributorsPerProblem(problemId, pagination).map {
-            val user = contributorService.findById(it.contributor).get()
+        val contributors = service.findContributorsPerProblem(problemId, pageable).map {
+            val user = contributorService.findById(ContributorId(it.contributor)).get()
             object {
                 val user = user
                 val contributions = it.freq
@@ -50,9 +49,9 @@ class ProblemController(
     fun getAuthorsPerProblem(
         @PathVariable problemId: ResourceId,
         @RequestParam("page", required = false) page: Int?,
-        @RequestParam("items", required = false) items: Int?
+        @RequestParam("items", required = false) items: Int?,
+        pageable: Pageable
     ): ResponseEntity<Iterable<Any>> {
-        val pagination = createPageable(page, items, null, false)
-        return ResponseEntity.ok(service.findAuthorsPerProblem(problemId, pagination))
+        return ResponseEntity.ok(service.findAuthorsPerProblem(problemId, pageable))
     }
 }

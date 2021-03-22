4678cc7a1ce474db2c3bbafef46afb3a87d3b387
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2021-03-22 16:27:57 +0100

    Implement new PwC service calls and remove dummy ones

--- src/main/kotlin/eu/tib/orkg/prototype/paperswithcode/application/service/PwcServices.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/paperswithcode/application/service/PwcServices.kt
@@ -5,65 +5,50 @@ import eu.tib.orkg.prototype.paperswithcode.application.domain.Dataset
 import eu.tib.orkg.prototype.paperswithcode.application.domain.DatasetSummary
 import eu.tib.orkg.prototype.paperswithcode.application.port.input.RetrieveBenchmarkUseCase
 import eu.tib.orkg.prototype.paperswithcode.application.port.input.RetrieveDatasetUseCase
-import eu.tib.orkg.prototype.paperswithcode.application.port.input.RetrieveResearchFieldUseCase
 import eu.tib.orkg.prototype.paperswithcode.application.port.output.FindDatasetsQuery
-import eu.tib.orkg.prototype.paperswithcode.application.port.output.FindResearchFieldsQuery
-import eu.tib.orkg.prototype.paperswithcode.application.port.output.FindResearchProblemQuery
 import eu.tib.orkg.prototype.paperswithcode.application.port.output.SummarizeBenchmarkQuery
 import eu.tib.orkg.prototype.paperswithcode.application.port.output.SummarizeDatasetQuery
 import eu.tib.orkg.prototype.statements.domain.model.ProblemService
-import eu.tib.orkg.prototype.statements.domain.model.ResearchField
 import eu.tib.orkg.prototype.statements.domain.model.ResearchFieldService
 import eu.tib.orkg.prototype.statements.domain.model.ResourceId
+import eu.tib.orkg.prototype.statements.domain.model.ResourceService
 import java.util.Optional
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 
+@Primary
 @Service
-class DummyBenchmarkService(
-    private val findResearchProblem: FindResearchProblemQuery,
+class BenchmarkService(
     private val summarizeBenchmark: SummarizeBenchmarkQuery,
     private val researchFieldService: ResearchFieldService
 ) : RetrieveBenchmarkUseCase {
     override fun summariesForResearchField(id: ResourceId): Optional<List<BenchmarkSummary>> {
-        /*
-        // TODO: Verification should be done on real service
         val researchField = researchFieldService.findById(id)
         if (!researchField.isPresent)
             return Optional.empty()
-         */
-        // TODO: Might be better to do in one query?
-        val benchmarkSummaries = findResearchProblem.allByResearchField(id).map {
-            summarizeBenchmark.byResearchProblem(it.id).get()
-        }
-        return Optional.of(benchmarkSummaries)
+        return Optional.of(
+            summarizeBenchmark.byResearchField(researchField.get().id!!)
+        )
     }
 }
 
+@Primary
 @Service
-class DummyDatasetService(
+class DatasetService(
     private val findDatasets: FindDatasetsQuery,
     private val researchProblemService: ProblemService,
-    private val summarizeDataset: SummarizeDatasetQuery
+    private val summarizeDataset: SummarizeDatasetQuery,
+    private val resourceService: ResourceService
 ) : RetrieveDatasetUseCase {
     override fun forResearchProblem(id: ResourceId): Optional<List<Dataset>> {
-        /*
-        // TODO: Validate in real implementation
         val problem = researchProblemService.findById(id)
         if (!problem.isPresent) return Optional.empty()
-         */
-        return findDatasets.forResearchProblem(id)
+        return Optional.of(findDatasets.forResearchProblem(id))
     }
 
     override fun summaryFor(id: ResourceId): Optional<List<DatasetSummary>> {
-        // TODO: check that dataset exists
-        return summarizeDataset.by(id)
+        val dataset = resourceService.findById(id)
+        if (!dataset.isPresent) return Optional.empty()
+        return Optional.of(summarizeDataset.by(id))
     }
-}
-
-@Service
-class DummyResearchFieldService(
-    private val findResearchFields: FindResearchFieldsQuery
-) : RetrieveResearchFieldUseCase {
-    // FIXME: Integrate with ResearchFieldService?
-    override fun withBenchmarks(): List<ResearchField> = findResearchFields.withBenchmarks()
 }

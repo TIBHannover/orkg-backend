6f149dc1bce8184bd7b6223cdab98bde1ee5d325
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2021-03-22 16:26:47 +0100

    Merge new benchmark call with existing RF service

--- src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jResearchFieldService.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jResearchFieldService.kt
@@ -4,6 +4,8 @@ import eu.tib.orkg.prototype.auth.persistence.UserEntity
 import eu.tib.orkg.prototype.auth.service.UserRepository
 import eu.tib.orkg.prototype.contributions.domain.model.Contributor
 import eu.tib.orkg.prototype.contributions.domain.model.ContributorId
+import eu.tib.orkg.prototype.paperswithcode.application.port.input.RetrieveResearchFieldUseCase
+import eu.tib.orkg.prototype.statements.domain.model.ResearchField
 import eu.tib.orkg.prototype.statements.domain.model.ResearchFieldService
 import eu.tib.orkg.prototype.statements.domain.model.Resource
 import eu.tib.orkg.prototype.statements.domain.model.ResourceId
@@ -20,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional
 class Neo4jResearchFieldService(
     private val neo4jResearchFieldRepository: Neo4jResearchFieldRepository,
     private val userRepository: UserRepository
-) : ResearchFieldService {
+) : ResearchFieldService, RetrieveResearchFieldUseCase {
     override fun findById(id: ResourceId): Optional<Resource> =
         neo4jResearchFieldRepository
             .findById(id)
@@ -63,4 +65,8 @@ class Neo4jResearchFieldService(
 
     override fun getResearchProblemsExcludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource> =
         neo4jResearchFieldRepository.getProblemsExcludingSubFields(id, pageable).map(Neo4jResource::toResource)
+
+    override fun withBenchmarks(): List<ResearchField> =
+        neo4jResearchFieldRepository.findResearchFieldsWithBenchmarks()
+            .map { ResearchField(it.resourceId!!.value, it.label!!) }
 }

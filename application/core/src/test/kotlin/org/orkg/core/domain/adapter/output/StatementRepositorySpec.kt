b7e03a5d7f88306a793c45eec00855ecacc9695b
Manuel Prinz <manuel.prinz@tib.eu>
2021-04-22 15:57:24 +0200

    WIP

--- application/core/src/test/kotlin/org/orkg/core/domain/adapter/output/StatementRepositorySpec.kt
+++ application/core/src/test/kotlin/org/orkg/core/domain/adapter/output/StatementRepositorySpec.kt
@@ -1,11 +1,6 @@
 package org.orkg.core.domain.adapter.output
 
 import io.kotest.core.spec.style.DescribeSpec
-import org.orkg.core.adapter.output.StatementRepository
 
 class StatementRepositorySpec :
-    DescribeSpec({
-      val repo: StatementRepository = object : StatementRepository {}
-
-      describe("find statement") { describe("find by id") {} }
-    })
+    DescribeSpec({ describe("find statement") { describe("find by id") {} } })

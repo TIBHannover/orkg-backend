b44ba3b9263d47fb00908c8c62dca8c3cc073134
Manuel Prinz <manuel.prinz@tib.eu>
2021-07-12 14:49:12 +0200

    Revert "Add caching to predicate service"
    This reverts commit 95ef19ff5a0a778561b0307df8fb5185c55110c0.
    
    Closes: #319

--- src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jPredicateService.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jPredicateService.kt
@@ -12,7 +12,6 @@ import eu.tib.orkg.prototype.util.EscapedRegex
 import eu.tib.orkg.prototype.util.SanitizedWhitespace
 import eu.tib.orkg.prototype.util.WhitespaceIgnorantPattern
 import java.util.Optional
-import java.util.concurrent.ConcurrentHashMap
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -24,8 +23,6 @@ class Neo4jPredicateService(
     private val neo4jPredicateRepository: Neo4jPredicateRepository,
     private val neo4jPredicateIdGenerator: Neo4jPredicateIdGenerator
 ) : PredicateService {
-
-    private val predicateCache: MutableMap<PredicateId, Predicate> = ConcurrentHashMap(64)
 
     override fun create(label: String) = create(ContributorId.createUnknownContributor(), label)
 
@@ -57,7 +54,10 @@ class Neo4jPredicateService(
             .map(Neo4jPredicate::toPredicate)
     }
 
-    override fun findById(id: PredicateId): Optional<Predicate> = Optional.ofNullable(findPredicateCached(id))
+    override fun findById(id: PredicateId?): Optional<Predicate> =
+        neo4jPredicateRepository
+            .findByPredicateId(id)
+            .map(Neo4jPredicate::toPredicate)
 
     override fun findAllByLabel(label: String, pageable: Pageable): Page<Predicate> =
         neo4jPredicateRepository
@@ -93,17 +93,4 @@ class Neo4jPredicateService(
     private fun String.toSearchString() = "(?i).*${WhitespaceIgnorantPattern(EscapedRegex(SanitizedWhitespace(this)))}.*"
 
     private fun String.toExactSearchString() = "(?i)^${WhitespaceIgnorantPattern(EscapedRegex(SanitizedWhitespace(this)))}$"
-
-    private fun findPredicateCached(id: PredicateId): Predicate? {
-        val cached = predicateCache[id]
-        if (cached != null)
-            return cached
-        val persisted = neo4jPredicateRepository.findByPredicateId(id).map(Neo4jPredicate::toPredicate)
-        if (persisted.isPresent) {
-            val instance = persisted.get()
-            predicateCache[id] = instance
-            return instance
-        }
-        return null
-    }
 }

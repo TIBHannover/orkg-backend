06a26f3f58379d1d8d277ae3524681187b73fa5f
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-19 14:37:43 +0200

    Add Research Fields

--- adapters/output/core/src/main/kotlin/ResearchFieldPersistenceAdapter.kt
+++ adapters/output/core/src/main/kotlin/ResearchFieldPersistenceAdapter.kt
@@ -1,4 +1,55 @@
 package eu.tib.orkg.prototype.core.statements.adapters.output
 
-class ResearchFieldPersistenceAdapter {
+import eu.tib.orkg.prototype.contributions.domain.model.Contributor
+import eu.tib.orkg.prototype.contributions.domain.model.ContributorId
+import eu.tib.orkg.prototype.statements.domain.model.ResearchField
+import eu.tib.orkg.prototype.statements.domain.model.Resource
+import eu.tib.orkg.prototype.statements.domain.model.ResourceId
+import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jResearchFieldRepository
+import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jResource
+import eu.tib.orkg.prototype.statements.ports.ResearchFieldRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Component
+import java.util.Optional
+
+
+@Component
+class ResearchFieldPersistenceAdapter(
+    private val neo4jResearchFieldRepository: Neo4jResearchFieldRepository,
+): ResearchFieldRepository {
+    override fun findById(id: ResourceId): Optional<Resource> =
+        neo4jResearchFieldRepository
+            .findById(id)
+            .map(Neo4jResource::toResource)
+
+    override fun getResearchProblemsOfField(id: ResourceId, pageable: Pageable): Page<Any> {
+        return neo4jResearchFieldRepository.getResearchProblemsOfField(id, pageable)
+            .map {
+                object {
+                    val problem = it.problem.toResource()
+                    val papers = it.papers
+                }
+            }
+    }
+
+    override fun getResearchProblemsIncludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource> =
+        neo4jResearchFieldRepository.getProblemsIncludingSubFields(id, pageable).map(Neo4jResource::toResource)
+
+    override fun getPapersIncludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource> =
+        neo4jResearchFieldRepository.getPapersIncludingSubFields(id, pageable).map(Neo4jResource::toResource)
+
+    override fun getComparisonsIncludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource> =
+        neo4jResearchFieldRepository.getComparisonsIncludingSubFields(id, pageable).map(Neo4jResource::toResource)
+
+     override fun getPapersExcludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource> =
+        neo4jResearchFieldRepository.getPapersExcludingSubFields(id, pageable).map(Neo4jResource::toResource)
+
+    override fun getComparisonsExcludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource> =
+        neo4jResearchFieldRepository.getComparisonsExcludingSubFields(id, pageable).map(Neo4jResource::toResource)
+
+    override fun getResearchProblemsExcludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource> =
+        neo4jResearchFieldRepository.getProblemsExcludingSubFields(id, pageable).map(Neo4jResource::toResource)
+
 }
--- application/core/src/main/kotlin/eu/tib/orkg/prototype/statements/ports/ResearchFieldRepository.kt
+++ application/core/src/main/kotlin/eu/tib/orkg/prototype/statements/ports/ResearchFieldRepository.kt
@@ -1,4 +1,28 @@
 package eu.tib.orkg.prototype.statements.ports
 
+import eu.tib.orkg.prototype.contributions.domain.model.Contributor
+import eu.tib.orkg.prototype.statements.domain.model.Resource
+import eu.tib.orkg.prototype.statements.domain.model.ResourceId
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import java.util.Optional
+
 interface ResearchFieldRepository {
+
+    fun findById(id: ResourceId): Optional<Resource>
+
+    fun getResearchProblemsOfField(id: ResourceId, pageable: Pageable): Page<Any>
+
+    fun getResearchProblemsIncludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource>
+
+    fun getPapersIncludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource>
+
+    fun getComparisonsIncludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource>
+
+    fun getPapersExcludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource>
+
+    fun getComparisonsExcludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource>
+
+    fun getResearchProblemsExcludingSubFields(id: ResourceId, pageable: Pageable): Page<Resource>
+
 }

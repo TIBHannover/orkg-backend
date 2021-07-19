df8682f908d10eec1665159533b1a7c14c7460a3
Manuel Prinz <manuel.prinz@tib.eu>
2021-07-19 08:38:48 +0200

    Update resource repository method signatures

--- adapters/output/core/src/main/kotlin/ResourcePersistenceAdapter.kt
+++ adapters/output/core/src/main/kotlin/ResourcePersistenceAdapter.kt
@@ -8,18 +8,18 @@ import eu.tib.orkg.prototype.statements.ports.ResourceRepository
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
+import java.util.Optional
 
 @Component
-class ResourcePersistenceAdapter(
-    private val neo4jResourceRepository: Neo4jResourceRepository
-) : ResourceRepository {
+class ResourcePersistenceAdapter(private val neo4jResourceRepository: Neo4jResourceRepository) :
+    ResourceRepository {
 
-    override fun save(resource: Resource) {
-        TODO("Not yet implemented")
-    }
+    override fun save(resource: Resource): Resource =
+        neo4jResourceRepository.save(resource.toNeo4jResource()).toResource()
 
-    override fun findById(resourceId: ResourceId?): Resource? =
-        neo4jResourceRepository.findByResourceId(resourceId).map(Neo4jResource::toResource).orElse(null)
+    override fun findById(resourceId: ResourceId?): Optional<Resource> =
+        neo4jResourceRepository.findByResourceId(resourceId).map(Neo4jResource::toResource)
+
 }
 
 @Configuration
--- application/core/src/main/kotlin/eu/tib/orkg/prototype/statements/ports/Repositories.kt
+++ application/core/src/main/kotlin/eu/tib/orkg/prototype/statements/ports/Repositories.kt
@@ -2,8 +2,9 @@ package eu.tib.orkg.prototype.statements.ports
 
 import eu.tib.orkg.prototype.statements.domain.model.Resource
 import eu.tib.orkg.prototype.statements.domain.model.ResourceId
+import java.util.Optional
 
 interface ResourceRepository {
-    fun save(resource: Resource)
-    fun findById(resourceId: ResourceId?): Resource?
+    fun save(resource: Resource): Resource
+    fun findById(resourceId: ResourceId?): Optional<Resource>
 }

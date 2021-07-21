d33886ba0edeb51f93c2ffc579b01f4d41fa6e83
Manuel Prinz <manuel.prinz@tib.eu>
2021-07-21 11:14:18 +0200

    Add AssertJ to build dependencies of shared project

--- application/shared/build.gradle.kts
+++ application/shared/build.gradle.kts
@@ -3,4 +3,5 @@ plugins { id("org.orkg.kotlin-conventions") }
 dependencies {
   testImplementation(platform("org.junit:junit-bom:5.7.2"))
   testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation("org.assertj:assertj-core:3.16.1")
 }

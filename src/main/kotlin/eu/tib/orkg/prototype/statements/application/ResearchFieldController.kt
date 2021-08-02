55552dab482ed1e2865b4247743d8fdb72f39a48
Suhas Bhairav <suhasbhairav@gmail.com>
2021-08-02 10:58:00 +0200

    Add featured changes

--- src/main/kotlin/eu/tib/orkg/prototype/statements/application/ResearchFieldController.kt
+++ src/main/kotlin/eu/tib/orkg/prototype/statements/application/ResearchFieldController.kt
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
+import org.springframework.lang.Nullable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -112,8 +113,8 @@ class ResearchFieldController(
     @GetMapping("/{id}/subfields/papers")
     fun getPapersIncludingSubFields(
         @PathVariable id: ResourceId,
-        @RequestParam("featured", required = false, defaultValue = "null")
-        featured: Boolean,
+        @Nullable @RequestParam("featured")
+        featured: Boolean?,
         @RequestParam("unlisted", required = false, defaultValue = "false")
         unlisted: Boolean,
         pageable: Pageable

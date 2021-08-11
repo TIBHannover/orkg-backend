b42f621d4d33014dcd349e25167d11794b0fbf75
Manuel Prinz <manuel.prinz@tib.eu>
2021-08-11 18:03:42 +0200

    Change entity hierarchy of Neo4jCounter

--- adapters/output/core/src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jIdCounters.kt
+++ adapters/output/core/src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/Neo4jIdCounters.kt
@@ -7,6 +7,10 @@ import org.springframework.data.neo4j.core.schema.Property
 import org.springframework.data.neo4j.repository.Neo4jRepository
 
 abstract class Neo4jCounter {
+    @Id
+    @GeneratedValue
+    private var id: Long? = null
+
     @Property
     var counter: Long = 0L
 }
@@ -16,47 +20,27 @@ interface Neo4jIdCounterRepository<T : Neo4jCounter> : Neo4jRepository<T, Long> 
     override fun <S : T> save(entity: S): S
 }
 
-@Node("_ResourceIdCounter")
-data class Neo4jResourceIdCounter(
-    @Id
-    @GeneratedValue
-    private var id: Long? = null
-) : Neo4jCounter()
+@Node(primaryLabel = "_ResourceIdCounter")
+class Neo4jResourceIdCounter : Neo4jCounter()
 
 interface Neo4jResourceIdCounterRepository : Neo4jIdCounterRepository<Neo4jResourceIdCounter>
 
-@Node("_PredicateIdCounter")
-data class Neo4jPredicateIdCounter(
-    @Id
-    @GeneratedValue
-    private var id: Long? = null
-) : Neo4jCounter()
+@Node(primaryLabel = "_PredicateIdCounter")
+class Neo4jPredicateIdCounter : Neo4jCounter()
 
 interface Neo4jPredicateIdCounterRepository : Neo4jIdCounterRepository<Neo4jPredicateIdCounter>
 
-@Node("_LiteralIdCounter")
-data class Neo4jLiteralIdCounter(
-    @Id
-    @GeneratedValue
-    private var id: Long? = null
-) : Neo4jCounter()
+@Node(primaryLabel = "_LiteralIdCounter")
+class Neo4jLiteralIdCounter : Neo4jCounter()
 
 interface Neo4jLiteralIdCounterRepository : Neo4jIdCounterRepository<Neo4jLiteralIdCounter>
 
-@Node("_StatementIdCounter")
-data class Neo4jStatementIdCounter(
-    @Id
-    @GeneratedValue
-    private var id: Long? = null
-) : Neo4jCounter()
+@Node(primaryLabel = "_StatementIdCounter")
+class Neo4jStatementIdCounter : Neo4jCounter()
 
 interface Neo4jStatementIdCounterRepository : Neo4jIdCounterRepository<Neo4jStatementIdCounter>
 
-@Node("_ClassIdCounter")
-data class Neo4jClassIdCounter(
-    @Id
-    @GeneratedValue
-    private var id: Long? = null
-) : Neo4jCounter()
+@Node(primaryLabel = "_ClassIdCounter")
+class Neo4jClassIdCounter : Neo4jCounter()
 
 interface Neo4jClassIdCounterRepository : Neo4jIdCounterRepository<Neo4jClassIdCounter>

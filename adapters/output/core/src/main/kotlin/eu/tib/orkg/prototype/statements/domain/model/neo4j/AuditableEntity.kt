33716e697bda5aa4819eb452ce279d752dd0bea4
Manuel Prinz <manuel.prinz@tib.eu>
2021-08-12 10:08:58 +0200

    Use OffsetDateTimeConverter in AuditableEntity base class

--- adapters/output/core/src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/AuditableEntity.kt
+++ adapters/output/core/src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/neo4j/AuditableEntity.kt
@@ -1,9 +1,12 @@
 package eu.tib.orkg.prototype.statements.domain.model.neo4j
 
+import eu.tib.orkg.prototype.statements.domain.model.neo4j.mapping.OffsetDateTimeConverter
+import org.springframework.data.neo4j.core.convert.ConvertWith
 import org.springframework.data.neo4j.core.schema.Property
 import java.time.OffsetDateTime
 
 open class AuditableEntity(
     @Property("created_at")
+    @ConvertWith(converter = OffsetDateTimeConverter::class)
     var createdAt: OffsetDateTime? = OffsetDateTime.now()
 )

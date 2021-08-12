fcbce6a5653a4e98636ee731ef125490c3107c02
Manuel Prinz <manuel.prinz@tib.eu>
2021-08-12 16:59:22 +0200

    Hide thing ID in REST representation

--- application/core/src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/Thing.kt
+++ application/core/src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/Thing.kt
@@ -1,6 +1,9 @@
 package eu.tib.orkg.prototype.statements.domain.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 sealed interface Thing {
+    @get:JsonIgnore
     val thingId: String?
     val label: String
 }

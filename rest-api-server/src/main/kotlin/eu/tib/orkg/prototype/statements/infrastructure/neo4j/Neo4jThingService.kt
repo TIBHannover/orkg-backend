74db1a82d2bd411ef19f85535a7180e3bb132496
Manuel Prinz <manuel.prinz@tib.eu>
2021-09-17 16:30:28 +0200

    Decouple thing service and repository

--- rest-api-server/src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jThingService.kt
+++ rest-api-server/src/main/kotlin/eu/tib/orkg/prototype/statements/infrastructure/neo4j/Neo4jThingService.kt
@@ -2,7 +2,7 @@ package eu.tib.orkg.prototype.statements.infrastructure.neo4j
 
 import eu.tib.orkg.prototype.statements.domain.model.Thing
 import eu.tib.orkg.prototype.statements.domain.model.ThingService
-import eu.tib.orkg.prototype.statements.domain.model.neo4j.Neo4jThingRepository
+import eu.tib.orkg.prototype.statements.ports.ThingRepository
 import java.util.Optional
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -11,18 +11,12 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional
 class Neo4jThingService(
-    private val neo4jThingService: Neo4jThingRepository
+    private val thingRepository: ThingRepository
 ) : ThingService {
 
-    override fun findAll(): Iterable<Thing> =
-        neo4jThingService.findAll().map { it.toThing() }
+    override fun findAll(): Iterable<Thing> = thingRepository.findAll()
 
-    override fun findAll(pageable: Pageable): Iterable<Thing> =
-        neo4jThingService.findAll(pageable)
-            .map { it.toThing() }
-            .content
+    override fun findAll(pageable: Pageable): Iterable<Thing> = thingRepository.findAll(pageable)
 
-    override fun findById(id: String?): Optional<Thing> =
-        neo4jThingService.findByThingId(id)
-            .map { it.toThing() }
+    override fun findById(id: String?): Optional<Thing> = thingRepository.findById(id)
 }

2da3ca67647890aea97494e4aa4e42cb3234fb1e
Manuel Prinz <manuel.prinz@tib.eu>
2022-02-08 13:16:46 +0100

    Remove obsolete test dependencies

--- rest-api-server/build.gradle.kts
+++ rest-api-server/build.gradle.kts
@@ -84,9 +84,6 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 
-    testImplementation("org.neo4j:neo4j-ogm-embedded-driver")
-    testImplementation("org.neo4j:neo4j-ogm-embedded-native-types")
-    testImplementation("org.neo4j:neo4j:$neo4jVersion")
     // TestContainers
     testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
     testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")

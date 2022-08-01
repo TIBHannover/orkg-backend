16aa9ba9b3c50893f94b775a3b0808f91262f55c
Manuel Prinz <manuel@pinguinkiste.de>
2022-08-01 07:18:33 +0000

    Fix regression when fetching multiple predicates (!266)

--- rest-api-server/src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/internal/Neo4jPredicateRepository.kt
+++ rest-api-server/src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/internal/Neo4jPredicateRepository.kt
@@ -5,6 +5,7 @@ import java.util.Optional
 import java.util.stream.Stream
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.neo4j.annotation.Query
 import org.springframework.data.neo4j.repository.Neo4jRepository
 
 interface Neo4jPredicateRepository : Neo4jRepository<Neo4jPredicate, Long> {
@@ -25,5 +26,10 @@ interface Neo4jPredicateRepository : Neo4jRepository<Neo4jPredicate, Long> {
 
     fun findByPredicateId(id: PredicateId?): Optional<Neo4jPredicate>
 
+    // @Query was added manually because of a strange bug that it is not reproducible. It seems that the OGM generates
+    // a query containing a string literal when the set only has one element, which the driver refused as an invalid
+    // query (which it is). It only happens under certain circumstances which are not reproducible in a test. I checked
+    // the driver version and everything else that came to mind. No idea what is wrong. This seems to work. -- MP
+    @Query("""MATCH (n:`Predicate`) WHERE n.predicate_id in {0} RETURN n""")
     fun findAllByPredicateIdIn(ids: Set<PredicateId>): Iterable<Neo4jPredicate>
 }

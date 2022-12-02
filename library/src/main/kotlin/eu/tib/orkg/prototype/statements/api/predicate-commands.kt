acdf5505b2c8f30b243259124dfbb49a4fff89bd
Marcel Konrad <marcel.konrad@tib.eu>
2022-12-02 14:15:53 +0000

    New create method for predicates (!330)

--- library/src/main/kotlin/eu/tib/orkg/prototype/statements/api/predicate-commands.kt
+++ library/src/main/kotlin/eu/tib/orkg/prototype/statements/api/predicate-commands.kt
@@ -5,12 +5,20 @@ import eu.tib.orkg.prototype.statements.application.CreatePredicateRequest
 import eu.tib.orkg.prototype.statements.domain.model.PredicateId
 
 interface CreatePredicateUseCase {
+    fun create(command: CreateCommand): PredicateId
+
     // legacy methods:
     fun create(label: String): PredicateRepresentation
     fun create(userId: ContributorId, label: String): PredicateRepresentation
     fun create(request: CreatePredicateRequest): PredicateRepresentation
     fun create(userId: ContributorId, request: CreatePredicateRequest): PredicateRepresentation
     fun createIfNotExists(id: PredicateId, label: String)
+
+    data class CreateCommand(
+        val label: String,
+        val id: String? = null,
+        val contributorId: ContributorId? = null
+    )
 }
 
 interface UpdatePredicateUseCase {

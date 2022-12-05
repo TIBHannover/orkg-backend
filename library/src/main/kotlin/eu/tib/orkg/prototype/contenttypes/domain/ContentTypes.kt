9487e1c4eb99ce977beed148bc9164ab95382be5
Marcel Konrad <Marcel.Konrad@tib.eu>
2022-12-05 11:24:10 +0100

    Paper model

--- library/src/main/kotlin/eu/tib/orkg/prototype/contenttypes/domain/ContentTypes.kt
+++ library/src/main/kotlin/eu/tib/orkg/prototype/contenttypes/domain/ContentTypes.kt
@@ -1,5 +1,12 @@
 package eu.tib.orkg.prototype.contenttypes.domain
 
+import eu.tib.orkg.prototype.contributions.domain.model.ContributorId
+import eu.tib.orkg.prototype.statements.application.ExtractionMethod
+import eu.tib.orkg.prototype.statements.domain.model.ObservatoryId
+import eu.tib.orkg.prototype.statements.domain.model.OrganizationId
+import eu.tib.orkg.prototype.statements.domain.model.ResourceId
+import java.time.OffsetDateTime
+
 typealias ContentTypeId = String
 
 sealed interface ContentType {
@@ -10,9 +17,29 @@ class Contribution(override val id: ContentTypeId) : ContentType
 
 class Paper(
     override val id: ContentTypeId,
-    val title: ContentTypeId,
+    val title: String,
+    val researchField: ResourceId,
+    val identifiers: Map<String, String>,
+    val publicationInfo: PublicationInfo,
+    val authors: List<ResourceId>, // TODO ThingId? and/or Set?
+    val contributors: List<ContributorId>,
+    val observatories: List<ObservatoryId>,
+    val organizations: List<OrganizationId>,
+    val extractionMethod: ExtractionMethod,
+    val createdAt: OffsetDateTime,
+    val createdBy: OffsetDateTime,
+    val featured: Boolean,
+    val unlisted: Boolean,
+    val verified: Boolean
 ) : ContentType
 
 class ResearchProblem(override val id: ContentTypeId) : ContentType
 
 class Visualization(override val id: ContentTypeId) : ContentType
+
+data class PublicationInfo(
+    val publishedMonth: Int,
+    val publishedYear: Long,
+    val publishedIn: String?,
+    val downloadUrl: String?
+)

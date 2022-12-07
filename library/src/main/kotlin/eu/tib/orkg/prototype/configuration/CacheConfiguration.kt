0336e0733846181bf3dc06291a462dc8c0c74f95
Manuel Prinz <manuel@pinguinkiste.de>
2022-12-07 16:57:54 +0000

    Configure caches explicitely via CacheManager (!351)

--- library/src/main/kotlin/eu/tib/orkg/prototype/configuration/CacheConfiguration.kt
+++ library/src/main/kotlin/eu/tib/orkg/prototype/configuration/CacheConfiguration.kt
@@ -1,9 +1,20 @@
 package eu.tib.orkg.prototype.configuration
 
 import com.github.benmanes.caffeine.cache.Caffeine
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.CLASS_ID_TO_CLASS_CACHE
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.CLASS_ID_TO_CLASS_EXISTS_CACHE
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.LITERAL_ID_TO_LITERAL_CACHE
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.LITERAL_ID_TO_LITERAL_EXISTS_CACHE
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.PREDICATE_ID_TO_PREDICATE_CACHE
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.RESOURCE_ID_TO_RESOURCE_CACHE
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.RESOURCE_ID_TO_RESOURCE_EXISTS_CACHE
+import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
+typealias CaffeineBuilder = Caffeine<Any?, Any?>
 
 @Configuration
 @EnableCaching
@@ -15,5 +26,18 @@ class CacheConfiguration {
      * programmatically.
      */
     @Bean
-    fun caffeineConfig() = Caffeine.newBuilder().recordStats()
+    fun caffeineConfig(): CaffeineBuilder = Caffeine.newBuilder().recordStats()
+
+    @Bean
+    fun cacheManager(caffeine: CaffeineBuilder): CacheManager = CaffeineCacheManager().apply {
+        setCaffeine(caffeine)
+        setCacheNames(
+            setOf(
+                CLASS_ID_TO_CLASS_CACHE, CLASS_ID_TO_CLASS_EXISTS_CACHE,
+                LITERAL_ID_TO_LITERAL_CACHE, LITERAL_ID_TO_LITERAL_EXISTS_CACHE,
+                PREDICATE_ID_TO_PREDICATE_CACHE,
+                RESOURCE_ID_TO_RESOURCE_CACHE, RESOURCE_ID_TO_RESOURCE_EXISTS_CACHE,
+            )
+        )
+    }
 }

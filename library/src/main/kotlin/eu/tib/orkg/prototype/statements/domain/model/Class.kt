7b71936c9b205c4e6e2676d72f75d9c4d49b293a
Manuel Prinz <manuel.prinz@tib.eu>
2022-12-25 15:15:00 +0100

    Prevent serialization information from being overwritten

--- library/src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/Class.kt
+++ library/src/main/kotlin/eu/tib/orkg/prototype/statements/domain/model/Class.kt
@@ -11,12 +11,13 @@ data class Class(
     val uri: URI?,
     val createdAt: OffsetDateTime,
     val createdBy: ContributorId = ContributorId.createUnknownContributor(),
-    // This is added to replace @JsonTypeInfo on the Thing interface
-    val _class: String? = "class"
 ) : Thing {
     var description: String? = null
     override val thingId: ThingId = id!!
-    fun toClass(): Class = Class(id, label, uri, createdAt, createdBy, _class)
+
+    // This is added to replace @JsonTypeInfo on the Thing interface
+    @Suppress("PropertyName")
+    val _class: String = "class"
 }
 
 fun Class?.toOptional() = Optional.ofNullable(this)

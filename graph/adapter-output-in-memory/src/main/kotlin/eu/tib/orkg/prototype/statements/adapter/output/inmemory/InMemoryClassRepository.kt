2a8de770d5251cf1f397613350c8f467e95c56f8
Marcel Konrad <Marcel.Konrad@tib.eu>
2023-01-04 09:49:00 +0100

    Fully implement InMemoryClassRepository

--- graph/adapter-output-in-memory/src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/output/inmemory/InMemoryClassRepository.kt
+++ graph/adapter-output-in-memory/src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/output/inmemory/InMemoryClassRepository.kt
@@ -2,9 +2,9 @@ package eu.tib.orkg.prototype.statements.adapter.output.inmemory
 
 import eu.tib.orkg.prototype.statements.domain.model.Class
 import eu.tib.orkg.prototype.statements.domain.model.ClassId
+import eu.tib.orkg.prototype.statements.domain.model.toOptional
 import eu.tib.orkg.prototype.statements.spi.ClassRepository
 import java.util.*
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
 class InMemoryClassRepository : InMemoryRepository<ClassId, Class>(
@@ -16,40 +16,36 @@ class InMemoryClassRepository : InMemoryRepository<ClassId, Class>(
 
     override fun findByClassId(id: ClassId?): Optional<Class> = Optional.ofNullable(entities[id!!])
 
-    override fun findAllByClassId(id: Iterable<ClassId>, pageable: Pageable): Page<Class> {
-        TODO("Not yet implemented")
-    }
+    override fun findAllByClassId(id: Iterable<ClassId>, pageable: Pageable) =
+        findAllFilteredAndPaged(pageable) { id.contains(it.id!!) }
 
-    override fun findAllByLabel(label: String): Iterable<Class> {
-        TODO("Not yet implemented")
-    }
+    override fun findAllByLabel(label: String) = entities.values.filter { it.label == label }
 
-    override fun findAllByLabel(label: String, pageable: Pageable): Page<Class> {
-        TODO("Not yet implemented")
-    }
+    override fun findAllByLabel(label: String, pageable: Pageable) =
+        findAllFilteredAndPaged(pageable) { it.label == label }
 
-    override fun findAllByLabelMatchesRegex(label: String): Iterable<Class> {
-        TODO("Not yet implemented")
-    }
+    override fun findAllByLabelMatchesRegex(label: String) =
+        entities.values.filter { it.label.matches(Regex(label)) }
 
-    override fun findAllByLabelMatchesRegex(label: String, pageable: Pageable): Page<Class> {
-        TODO("Not yet implemented")
-    }
+    override fun findAllByLabelMatchesRegex(label: String, pageable: Pageable) =
+        findAllFilteredAndPaged(pageable) { it.label.matches(Regex(label)) }
 
-    override fun findAllByLabelContaining(part: String): Iterable<Class> {
-        TODO("Not yet implemented")
-    }
+    override fun findAllByLabelContaining(part: String) =
+        entities.values.filter { it.label.contains(part) }
 
-    override fun findByUri(uri: String): Optional<Class> {
-        TODO("Not yet implemented")
-    }
+    override fun findByUri(uri: String) =
+        entities.values.firstOrNull { it.uri.toString() == uri }.toOptional()
 
     override fun deleteAll() {
         entities.clear()
     }
 
     override fun nextIdentity(): ClassId {
-        TODO("Not yet implemented")
+        var id = ClassId(entities.size.toLong())
+        while(entities.contains(id)) {
+            id = ClassId(id.value.toLong() + 1)
+        }
+        return id
     }
 
     override fun existsAll(ids: Set<ClassId>): Boolean =

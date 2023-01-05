2fde09201f9f5ac38c33c17b8b08ca710bb64baf
Marcel Konrad <Marcel.Konrad@tib.eu>
2023-01-05 16:42:40 +0100

    Add missing bootstrap classes for spring data neo4j repository contract tests

--- rest-api-server/src/integration/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/SpringDataNeo4jPredicateAdapterContractTests.kt
+++ rest-api-server/src/integration/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/SpringDataNeo4jPredicateAdapterContractTests.kt
@@ -1,11 +1,20 @@
 package eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring
 
+import eu.tib.orkg.prototype.Neo4jContainerInitializer
+import eu.tib.orkg.prototype.configuration.CacheConfiguration
+import eu.tib.orkg.prototype.configuration.Neo4jConfiguration
 import eu.tib.orkg.prototype.statements.spi.PredicateRepository
 import eu.tib.orkg.prototype.statements.spi.PredicateRepositoryContractTest
 import eu.tib.orkg.prototype.statements.spi.ResourceRepository
 import eu.tib.orkg.prototype.statements.spi.StatementRepository
+import eu.tib.orkg.prototype.statements.spi.predicateRepositoryContract
 import eu.tib.orkg.prototype.testing.Neo4jTestContainersBaseTest
+import io.kotest.core.spec.style.DescribeSpec
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
@@ -35,3 +44,13 @@ class SpringDataNeo4jPredicateAdapterContractTests : Neo4jTestContainersBaseTest
         resourceRepository.deleteAll()
     }
 }
+
+@DataNeo4jTest
+@ContextConfiguration(classes = [SpringDataNeo4jPredicateAdapter::class], initializers = [Neo4jContainerInitializer::class])
+@Import(Neo4jConfiguration::class, CacheConfiguration::class)
+@ComponentScan(basePackages = ["eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.internal"])
+internal class SpringDataNeo4jPredicateAdapterContractTestsKotest(
+    @Autowired private val springDataNeo4jPredicateAdapter: PredicateRepository,
+) : DescribeSpec({
+    include(predicateRepositoryContract(springDataNeo4jPredicateAdapter))
+})
--- rest-api-server/src/integration/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/SpringDataNeo4jResourceAdapterContractTests.kt
+++ rest-api-server/src/integration/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/SpringDataNeo4jResourceAdapterContractTests.kt
@@ -1,9 +1,18 @@
 package eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring
 
+import eu.tib.orkg.prototype.Neo4jContainerInitializer
+import eu.tib.orkg.prototype.configuration.CacheConfiguration
+import eu.tib.orkg.prototype.configuration.Neo4jConfiguration
 import eu.tib.orkg.prototype.statements.spi.ResourceRepository
 import eu.tib.orkg.prototype.statements.spi.ResourceRepositoryContractTest
+import eu.tib.orkg.prototype.statements.spi.resourceRepositoryContract
 import eu.tib.orkg.prototype.testing.Neo4jTestContainersBaseTest
+import io.kotest.core.spec.style.DescribeSpec
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
@@ -19,3 +28,13 @@ class SpringDataNeo4jResourceAdapterContractTests : Neo4jTestContainersBaseTest(
         repository.deleteAll()
     }
 }
+
+@DataNeo4jTest
+@ContextConfiguration(classes = [SpringDataNeo4jResourceAdapter::class], initializers = [Neo4jContainerInitializer::class])
+@Import(Neo4jConfiguration::class, CacheConfiguration::class)
+@ComponentScan(basePackages = ["eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.internal"])
+internal class SpringDataNeo4jResourceAdapterContractTestsKotest(
+    @Autowired private val springDataNeo4jResourceAdapter: ResourceRepository
+) : DescribeSpec({
+    include(resourceRepositoryContract(springDataNeo4jResourceAdapter))
+})

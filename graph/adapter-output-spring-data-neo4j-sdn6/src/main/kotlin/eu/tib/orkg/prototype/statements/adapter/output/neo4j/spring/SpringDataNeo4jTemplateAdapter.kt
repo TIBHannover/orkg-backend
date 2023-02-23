1be271a55ac09a3a00dbe3c29453335652bc2a81
Marcel Konrad <marcel.konrad@tib.eu>
2023-02-23 14:51:21 +0000

    Add Template adapter for SDN6 (!449)

---
+++ graph/adapter-output-spring-data-neo4j-sdn6/src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/SpringDataNeo4jTemplateAdapter.kt
@@ -0,0 +1,26 @@
+package eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring
+
+import dev.forkhandles.values.ofOrNull
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.internal.Neo4jTemplateRepository
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.internal.TemplatedResource
+import eu.tib.orkg.prototype.statements.domain.model.FormattedLabel
+import eu.tib.orkg.prototype.statements.domain.model.ThingId
+import eu.tib.orkg.prototype.statements.spi.TemplateRepository
+import java.util.*
+import org.springframework.stereotype.Component
+
+@Component
+class SpringDataNeo4jTemplateAdapter(
+    private val neo4jRepository: Neo4jTemplateRepository
+) : TemplateRepository {
+
+    override fun findTemplateSpecs(resourceId: ThingId): Optional<TemplatedResource> =
+        neo4jRepository.findTemplateSpecs(resourceId.toResourceId())
+
+    override fun formattedLabelFor(resourceId: ThingId, classes: Set<ThingId>): FormattedLabel? {
+        if (classes.isEmpty()) return null
+        val templatedResource = findTemplateSpecs(resourceId)
+        if (!templatedResource.isPresent) return null
+        return FormattedLabel.ofOrNull(templatedResource.get().composeFormattedLabel())
+    }
+}

55e1610cf22db092c3c676d1289c8f91d18ccabd
Marcel Konrad <marcel.konrad@tib.eu>
2023-02-23 14:17:15 +0000

    Add Contribution adapter for SDN6 (!444)

---
+++ graph/adapter-output-spring-data-neo4j-sdn6/src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/SpringDataNeo4jContributionAdapter.kt
@@ -0,0 +1,31 @@
+package eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring
+
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.internal.Neo4jContributionRepository
+import eu.tib.orkg.prototype.statements.domain.model.Resource
+import eu.tib.orkg.prototype.statements.domain.model.ThingId
+import eu.tib.orkg.prototype.statements.spi.ContributionRepository
+import java.util.*
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Component
+
+@Component
+class SpringDataNeo4jContributionAdapter(
+    private val neo4jRepository: Neo4jContributionRepository
+) : ContributionRepository {
+
+    override fun findContributionByResourceId(id: ThingId): Optional<Resource> =
+        neo4jRepository.findContributionByResourceId(id.toResourceId()).map { it.toResource() }
+
+    override fun findAllFeaturedContributions(pageable: Pageable): Page<Resource> =
+        neo4jRepository.findAllFeaturedContributions(pageable).map { it.toResource() }
+
+    override fun findAllNonFeaturedContributions(pageable: Pageable): Page<Resource> =
+        neo4jRepository.findAllNonFeaturedContributions(pageable).map { it.toResource() }
+
+    override fun findAllUnlistedContributions(pageable: Pageable): Page<Resource> =
+        neo4jRepository.findAllUnlistedContributions(pageable).map { it.toResource() }
+
+    override fun findAllListedContributions(pageable: Pageable): Page<Resource> =
+        neo4jRepository.findAllListedContributions(pageable).map { it.toResource() }
+}
---
+++ graph/adapter-output-spring-data-neo4j-sdn6/src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/internal/Neo4jContributionRepository.kt
@@ -0,0 +1,75 @@
+package eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.internal
+
+import eu.tib.orkg.prototype.statements.domain.model.ResourceId
+import java.util.*
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.neo4j.repository.Neo4jRepository
+import org.springframework.data.neo4j.repository.query.Query
+
+private const val id = "${'$'}id"
+
+/**
+ * Partial query that returns the node as well as its ID and relationships.
+ * Queries using this partial query must use `node` as the binding name.
+ */
+private const val RETURN_NODE =
+    """RETURN node, [ [ (node)<-[r_r1:`RELATED`]-(r1:`Resource`) | [ r_r1, r1 ] ], [ (node)-[r_r1:`RELATED`]->(r1:`Resource`) | [ r_r1, r1 ] ] ], ID(node)"""
+
+/**
+ * Partial query that returns the node count for use in count queries.
+ * Queries using this partial query must use `node` as the binding name.
+ */
+private const val RETURN_NODE_COUNT = """RETURN count(node)"""
+
+/**
+ * Partial query that expands the node properties so that they can be used with pagination in custom queries.
+ * Queries using this partial query must use `node` as the binding name.
+ */
+private const val WITH_NODE_PROPERTIES =
+    """WITH node, node.label AS label, node.resource_id AS id, node.created_at AS created_at"""
+
+private const val MATCH_FEATURED_CONTRIBUTION =
+    """MATCH (node) WHERE EXISTS(node.featured) AND node.featured = true AND ANY(collectionFields IN ['Contribution'] WHERE collectionFields IN LABELS(node))"""
+
+private const val MATCH_NONFEATURED_CONTRIBUTION =
+    """MATCH (node) WHERE (NOT EXISTS(node.featured) OR node.featured = false) AND ANY(collectionFields IN ['Contribution'] WHERE collectionFields IN LABELS(node))"""
+
+private const val MATCH_UNLISTED_CONTRIBUTION =
+    """MATCH (node) WHERE EXISTS(node.unlisted) AND node.unlisted = true AND ANY(collectionFields IN ['Contribution'] WHERE collectionFields IN LABELS(node))"""
+
+private const val MATCH_LISTED_CONTRIBUTION =
+    """MATCH (node) WHERE (NOT EXISTS(node.unlisted) OR node.unlisted = false) AND ANY(collectionFields IN ['Contribution'] WHERE collectionFields IN LABELS(node))"""
+
+private const val MATCH_CONTRIBUTION_BY_ID = """MATCH (node:`Resource`:`Contribution` {resource_id: $id})"""
+
+interface Neo4jContributionRepository :
+    Neo4jRepository<Neo4jResource, Long> {
+
+    @Query("""$MATCH_CONTRIBUTION_BY_ID $WITH_NODE_PROPERTIES $RETURN_NODE""")
+    fun findContributionByResourceId(id: ResourceId): Optional<Neo4jResource>
+
+    @Query(
+        value = """$MATCH_FEATURED_CONTRIBUTION $WITH_NODE_PROPERTIES $RETURN_NODE""",
+        countQuery = """$MATCH_FEATURED_CONTRIBUTION $WITH_NODE_PROPERTIES $RETURN_NODE_COUNT"""
+    )
+    fun findAllFeaturedContributions(pageable: Pageable): Page<Neo4jResource>
+
+    @Query(
+        value = """$MATCH_NONFEATURED_CONTRIBUTION $WITH_NODE_PROPERTIES $RETURN_NODE""",
+        countQuery = """$MATCH_NONFEATURED_CONTRIBUTION $WITH_NODE_PROPERTIES $RETURN_NODE_COUNT"""
+    )
+    fun findAllNonFeaturedContributions(pageable: Pageable): Page<Neo4jResource>
+
+    @Query(
+        value = """$MATCH_UNLISTED_CONTRIBUTION $WITH_NODE_PROPERTIES $RETURN_NODE""",
+        countQuery = """$MATCH_UNLISTED_CONTRIBUTION $WITH_NODE_PROPERTIES $RETURN_NODE_COUNT"""
+    )
+    fun findAllUnlistedContributions(pageable: Pageable): Page<Neo4jResource>
+
+    @Query(
+        value = """$MATCH_LISTED_CONTRIBUTION $WITH_NODE_PROPERTIES $RETURN_NODE""",
+        countQuery = """$MATCH_LISTED_CONTRIBUTION $WITH_NODE_PROPERTIES $RETURN_NODE_COUNT"""
+    )
+    fun findAllListedContributions(pageable: Pageable): Page<Neo4jResource>
+}

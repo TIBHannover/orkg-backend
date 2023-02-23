3860bbe93ee5047531181f1e2b11b3fe3df45cf1
Marcel Konrad <marcel.konrad@tib.eu>
2023-02-23 14:49:10 +0000

    Add SmartReview adapter for SDN6 (!447)

---
+++ graph/adapter-output-spring-data-neo4j-sdn6/src/main/kotlin/eu/tib/orkg/prototype/statements/adapter/output/neo4j/spring/SpringDataNeo4jSmartReviewAdapter.kt
@@ -0,0 +1,30 @@
+package eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring
+
+import eu.tib.orkg.prototype.statements.adapter.output.neo4j.spring.internal.Neo4jSmartReviewRepository
+import eu.tib.orkg.prototype.statements.domain.model.Resource
+import eu.tib.orkg.prototype.statements.domain.model.ThingId
+import eu.tib.orkg.prototype.statements.spi.SmartReviewRepository
+import java.util.*
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Component
+
+@Component
+class SpringDataNeo4jSmartReviewAdapter(
+    private val neo4jRepository: Neo4jSmartReviewRepository
+) : SmartReviewRepository {
+    override fun findSmartReviewByResourceId(id: ThingId): Optional<Resource> =
+        neo4jRepository.findSmartReviewByResourceId(id.toResourceId()).map { it.toResource() }
+
+    override fun findAllFeaturedSmartReviews(pageable: Pageable): Page<Resource> =
+        neo4jRepository.findAllFeaturedSmartReviews(pageable).map { it.toResource() }
+
+    override fun findAllNonFeaturedSmartReviews(pageable: Pageable): Page<Resource> =
+        neo4jRepository.findAllNonFeaturedSmartReviews(pageable).map { it.toResource() }
+
+    override fun findAllUnlistedSmartReviews(pageable: Pageable): Page<Resource> =
+        neo4jRepository.findAllUnlistedSmartReviews(pageable).map { it.toResource() }
+
+    override fun findAllListedSmartReviews(pageable: Pageable): Page<Resource> =
+        neo4jRepository.findAllListedSmartReviews(pageable).map { it.toResource() }
+}

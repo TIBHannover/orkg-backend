cb4e96dee631a5ae1e26291a7a6aa617685f3c2b
Manuel Prinz <manuel.prinz@tib.eu>
2023-06-23 08:53:37 +0200

    Add comment on authors

--- graph/graph-application/src/main/kotlin/eu/tib/orkg/prototype/contenttypes/domain/model/Author.kt
+++ graph/graph-application/src/main/kotlin/eu/tib/orkg/prototype/contenttypes/domain/model/Author.kt
@@ -2,6 +2,8 @@ package eu.tib.orkg.prototype.contenttypes.domain.model
 
 import eu.tib.orkg.prototype.statements.domain.model.ThingId
 
+// TODO: convert to representation and merge with the one from statements package?
+//       could use an abstraction of AuthorList (domain object) instead of List<Author> in Paper.
 data class Author(
     val id: ThingId?,
     val name: String,

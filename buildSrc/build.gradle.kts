161075ee69a008771f02ca0dbdcc5aeb3ca63fff
Manuel Prinz <manuel@pinguinkiste.de>
2023-09-27 14:24:48 +0000

    Reorganize Gradle plugins (!651)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.gradle.kotlin.dsl.accessors.runtime.conventionOf
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -43,16 +42,4 @@ gradlePlugin {
             implementationClass = "org.orkg.gradle.plugins.PrintCoveragePlugin"
         }
     }
-}
-
-// Compile Groovy before Kotlin (https://docs.gradle.org/6.1/release-notes.html#compilation-order)
-tasks.named<GroovyCompile>("compileGroovy") {
-    // Groovy only needs the declared dependencies
-    // and not the output of compileJava
-    classpath = sourceSets.main.get().compileClasspath
-}
-tasks.named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
-    // Kotlin also depends on the result of Groovy compilation
-    // which automatically makes it depend on compileGroovy
-    classpath += files(conventionOf(sourceSets.main.get()).getPlugin(GroovySourceSet::class.java).groovy.classesDirectory)
 }

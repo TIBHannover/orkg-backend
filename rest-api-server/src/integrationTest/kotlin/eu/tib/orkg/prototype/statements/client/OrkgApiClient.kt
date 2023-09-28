0d31601d71ba9d4099111d34267c4874555d5a98
Manuel Prinz <manuel.prinz@tib.eu>
2023-09-28 16:14:29 +0200

    Replace old implementation

--- rest-api-server/src/integrationTest/kotlin/eu/tib/orkg/prototype/statements/client/OrkgApiClient.kt
+++ rest-api-server/src/integrationTest/kotlin/eu/tib/orkg/prototype/statements/client/OrkgApiClient.kt
@@ -1,6 +1,5 @@
 package eu.tib.orkg.prototype.statements.client
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SNAKE_CASE
@@ -15,7 +14,6 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED
 import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.util.LinkedMultiValueMap
-import org.springframework.util.MultiValueMap
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForObject
 
@@ -40,22 +38,10 @@ class OrkgApiClient(private val port: Int = 80) {
     }
 
     /**
-     * Obtain an access token by logging in.
+     * Obtain a new access token.
      */
-    fun getAccessToken(username: String, password: String, path: String): String? {
-        // TODO: externalize settings
-        val uri = URI("http", null, "localhost", port, path, "client_id=$clientId&response_type=code", null)
-        val url = uri.toString()
-
-        val params = getRequestParameters(username, password)
-        val headers = getRequestHeaders()
-        val request = HttpEntity(params, headers)
-
-        val restTemplate = RestTemplate().apply {
-            messageConverters.add(StringHttpMessageConverter())
-        }
-        return restTemplate.postForObject<TokenResponse>(url, request).accessToken
-    }
+    fun getAccessToken(username: String, password: String, path: String): String? =
+        tokensFor(username, password).accessToken
 
     /**
      * Obtain tokens from via the token endpoint.
@@ -72,12 +58,6 @@ class OrkgApiClient(private val port: Int = 80) {
             .`when`().post(oidc.tokenEndpoint)
             .then().statusCode(200)
             .extract().`as`(TokenResponse::class.java, objectMapper)
-
-    private fun getRequestHeaders() =
-        HttpHeaders().apply {
-            add("Authorization", "Basic ${encodeCredentials()}")
-            contentType = APPLICATION_FORM_URLENCODED
-        }
 
     fun registerUser(username: String, password: String, path: String) {
         val uri = URI("http", null, "localhost", port, path, "client_id=orkg-frontend&response_type=code", null)
@@ -102,33 +82,13 @@ class OrkgApiClient(private val port: Int = 80) {
         restTemplate.postForObject<String?>(url, request)
     }
 
-    private fun getRequestParameters(
-        username: String,
-        password: String
-    ): MultiValueMap<String, String> =
-        LinkedMultiValueMap<String, String>().apply {
-            add("grant_type", "password")
-            add("client_id", clientId)
-            add("username", username)
-            add("password", password)
-        }
-
     /**
-     * Helper function to encode client credentials in Base64.
-     */
-    private fun encodeCredentials() = Base64.getEncoder().encodeToString("$clientId:$clientSecret".toByteArray())
-
-    /**
-     * Helper class to provide access to the elements in the OAuth2 token response.
+     * Provides access to the elements in the OIDC token response.
      */
     data class TokenResponse(
-        @JsonProperty("access_token")
         val accessToken: String?,
-        @JsonProperty("token_type")
         val tokenType: String?,
-        @JsonProperty("expires_in")
         val expiresIn: String?,
-        @JsonProperty("scope")
         val scope: String?
     )
 }

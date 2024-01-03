2607fda2533647d487190ee317795fb62f5ab073
Manuel Prinz <manuel.prinz@tib.eu>
2024-01-03 08:17:14 +0100

    Convert JUnit4 test to JUnit5

--- rest-api-server/src/test/kotlin/org/orkg/ConfigurationFileParserTest.kt
+++ rest-api-server/src/test/kotlin/org/orkg/ConfigurationFileParserTest.kt
@@ -1,22 +1,25 @@
 package org.orkg
 
 import java.io.FileReader
-import org.junit.Test
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.autoconfigure.json.JsonTest
 
+@JsonTest // We need application.yaml loaded for @Value to work. This application context will do, as it is small.
 class ConfigurationFileParserTest {
 
-    @Value("orkg.init.setup.entitiesFile")
-    private var filename: String? = null
+    @Value("\${orkg.init.setup.entities-file:#{null}}")
+    private var configuredFilename: String? = null
 
     @Test
-    fun testParseInitialData() {
-        val fileParser = EntityConfigurationParser(
-            FileReader(
-                this::class.java.classLoader.getResource(filename).file
-            )
-        )
-        Assertions.assertDoesNotThrow(fileParser::parseInitialData)
+    fun verifyInitialDataCanBeLoadedFromConfigurationAndIsFreeOfErrors() {
+        if (configuredFilename == null) throw IllegalStateException("Filename was not provided or could not be read")
+        val filename: String = this::class.java.classLoader.getResource(configuredFilename)?.file
+            ?: throw IllegalStateException("Filename was configured, but file was not found")
+
+        val fileParser = EntityConfigurationParser(FileReader(filename))
+
+        assertDoesNotThrow(fileParser::parseInitialData)
     }
 }

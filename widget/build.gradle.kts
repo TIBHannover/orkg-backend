ea3e3091f5c06944f0e7945f95fc2f6aeb433716
Manuel Prinz <manuel@pinguinkiste.de>
2024-04-29 15:12:25 +0000

    Clean dependencies of widget module

--- widget/build.gradle.kts
+++ widget/build.gradle.kts
@@ -5,14 +5,12 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":common"))
-    implementation(project(":common:serialization"))
+    api("com.fasterxml.jackson.core:jackson-annotations")
+    api("org.springframework:spring-context")
+    api("org.springframework:spring-web")
+    api(project(":common"))
+    api(project(":graph:graph-ports-input"))
     implementation(project(":graph:graph-core-model"))
-    implementation(project(":graph:graph-ports-input"))
-
-    implementation("org.springframework:spring-context")
-    implementation("com.fasterxml.jackson.core:jackson-databind")
-    implementation("org.springframework:spring-web")
 
     testApi(enforcedPlatform(libs.junit5.bom)) // TODO: can be removed after upgrade to Spring Boot 2.7
 }
@@ -21,20 +19,32 @@ testing {
     suites {
         val test by getting(JvmTestSuite::class) {
             dependencies {
-                implementation(testFixtures(project(":graph:graph-core-model")))
-                implementation(libs.kotest.runner)
-                implementation(libs.spring.mockk)
-                implementation(libs.kotest.framework.datatest)
-
-                implementation(testFixtures(project(":testing:spring")))
-                implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // to (de)serialize data classes
-                implementation("org.springframework.boot:spring-boot-starter-web")
+                implementation("io.kotest:kotest-assertions-api")
+                implementation("io.kotest:kotest-assertions-shared")
+                implementation("io.kotest:kotest-common")
+                implementation("io.kotest:kotest-framework-api")
+                implementation("io.mockk:mockk-dsl")
+                implementation("io.mockk:mockk-jvm")
+                implementation("org.hamcrest:hamcrest")
+                implementation("org.junit.jupiter:junit-jupiter-api")
                 implementation("org.springframework.boot:spring-boot-starter-test") {
                     exclude(group = "junit", module = "junit")
                     exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
                     exclude(module = "mockito-core")
                 }
+                implementation("org.springframework.boot:spring-boot-starter-web")
+                implementation("org.springframework.boot:spring-boot-test-autoconfigure")
+                implementation("org.springframework.restdocs:spring-restdocs-core")
+                implementation("org.springframework:spring-test")
+                implementation(libs.kotest.assertions.core)
+                implementation(libs.kotest.framework.datatest)
+                implementation(libs.kotest.runner)
+                implementation(libs.spring.mockk)
                 implementation(libs.spring.restdocs)
+                implementation(project(":common:serialization"))
+                implementation(testFixtures(project(":graph:graph-core-model")))
+                implementation(testFixtures(project(":testing:spring")))
+                runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin") // to (de)serialize data classes
             }
         }
     }

866bb83acda3240ef5c23610a90c64d8aa8c37f2
Marcel Konrad <Marcel.Konrad@tib.eu>
2024-06-18 11:40:38 +0200

    Use string templates for logging

--- content-types/content-types-adapter-input-rest-spring-mvc/src/main/kotlin/org/orkg/contenttypes/adapter/input/rest/PaperRanker.kt
+++ content-types/content-types-adapter-input-rest-spring-mvc/src/main/kotlin/org/orkg/contenttypes/adapter/input/rest/PaperRanker.kt
@@ -47,13 +47,13 @@ class PaperRanker(
                     visibility = Visibility.DEFAULT,
                     unlistedBy = null
                 ))
-                logger.debug("Re-listing paper \"${paper.id}\".")
+                logger.debug("""Re-listing paper "{}".""", paper.id)
             } else if (paper.visibility == Visibility.DEFAULT && !score.isSufficient) {
                 resourceRepository.save(paper.copy(
                     visibility = Visibility.UNLISTED,
                     unlistedBy = ContributorId.SYSTEM
                 ))
-                logger.debug("Unlisting paper \"${paper.id}\".")
+                logger.debug("""Unlisting paper "{}".""", paper.id)
             }
         }.thenRun {
             logger.info("Done ranking papers. Took ${(System.nanoTime() - startMillis).toMillis()}ms")

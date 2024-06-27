ef423837fcfe4e4b6ffcbdea905eebb6fc4887a4
John <Lena.John@tib.eu>
2024-06-27 11:48:05 +0200

    new endpoint for classes without descr

--- curation/curation-ports-output/src/main/kotlin/org/orkg/curation/output/CurationRepository.kt
+++ curation/curation-ports-output/src/main/kotlin/org/orkg/curation/output/CurationRepository.kt
@@ -1,9 +1,11 @@
 package org.orkg.curation.output
 
+import org.orkg.graph.domain.Class
 import org.orkg.graph.domain.Predicate
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
 interface CurationRepository {
     fun findAllPredicatesWithoutDescriptions(pageable: Pageable): Page<Predicate>
+    fun findAllClassesWithoutDescriptions(pageable: Pageable): Page<Class>
 }

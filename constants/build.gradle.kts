9bf6b1f1b320123f23baf44a2df02e0e8f79bf7d
Marcel Konrad <marcel.konrad@tib.eu>
2025-02-13 14:38:42 +0000

    Update to Keycloak 26.1.2

--- constants/build.gradle.kts
+++ constants/build.gradle.kts
@@ -12,5 +12,5 @@ buildConfig {
     buildConfigField("APP_NAME", rootProject.name)
     buildConfigField("CONTAINER_IMAGE_NEO4J", "neo4j:5-community")
     buildConfigField("CONTAINER_IMAGE_POSTGRES", "postgres:17")
-    buildConfigField("CONTAINER_IMAGE_KEYCLOAK", "registry.gitlab.com/tibhannover/orkg/keycloak-docker-image:26.0.5")
+    buildConfigField("CONTAINER_IMAGE_KEYCLOAK", "registry.gitlab.com/tibhannover/orkg/keycloak-docker-image:26")
 }

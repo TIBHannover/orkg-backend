d5974f9f86363684792354c38e773560cdc48332
Manuel Prinz <manuel@pinguinkiste.de>
2025-03-20 11:03:30 +0100

    Ignore included builds in automatic project scanning (!1275)

--- gradle/meta-plugins/settings-plugins/src/main/kotlin/org.orkg.gradle.project-structure.settings.gradle.kts
+++ gradle/meta-plugins/settings-plugins/src/main/kotlin/org.orkg.gradle.project-structure.settings.gradle.kts
@@ -1,4 +1,7 @@
-// Include all subfolders that contain a 'build.gradle.kts' as subprojects
-rootDir.listFiles()?.filter { File(it, "build.gradle.kts").exists() }?.forEach { subproject ->
-    include(subproject.name)
-}
+// Include all subfolders that contain a 'build.gradle.kts' as subprojects, if they are not part of an included build
+rootDir
+    .listFiles()
+    ?.filter { File(it, "settings.gradle.kts").exists().not() && File(it, "build.gradle.kts").exists() }
+    ?.forEach { subproject ->
+        include(subproject.name)
+    }

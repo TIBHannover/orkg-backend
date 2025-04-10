ce0f633502f5153edaf787369577dc160a0caa5d
Marcel Konrad <marcel.konrad@tib.eu>
2025-04-10 08:53:27 +0000

    Add config option for thread pool size of task scheduler (!2196)

---
+++ rest-api-server/src/main/kotlin/org/orkg/configuration/TaskSchedulerConfiguration.kt
@@ -0,0 +1,15 @@
+package org.orkg.configuration
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+
+@Configuration
+class TaskSchedulerConfiguration {
+    @Bean
+    fun taskScheduler(
+        @Value("\${orkg.concurrent.task-scheduler-thead-pool-size:#{3}}") threadPoolSize: Int,
+    ): ThreadPoolTaskScheduler =
+        ThreadPoolTaskScheduler().apply { poolSize = threadPoolSize }
+}
